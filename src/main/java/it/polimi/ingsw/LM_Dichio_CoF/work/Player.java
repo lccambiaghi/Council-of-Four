8d11a239c424173b824cf681c7e9994bbfece53f
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-17 21:48:43 +0200

    Ha qualche variabile in più, come il carattere che definisce il tipo di connessione e l'indirizzo IP

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Player.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Player.java
@@ -2,4 +2,36 @@ package it.polimi.ingsw.LM_Dichio_CoF.work;
 
 public class Player {
 
+	String IP;
+	String id;
+	String nickname;
+	
+	char typeOfConnection;
+
+	public Player(char typeOfConnection, String IP){
+		
+		this.typeOfConnection=typeOfConnection;
+		this.IP=IP;
+		
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public char getTypeOfConnection() {
+		return typeOfConnection;
+	}
+
+	public void setTypeOfConnection(char typeOfConnection) {
+		this.typeOfConnection = typeOfConnection;
+	}
+	
+	
+	
+	
 }
