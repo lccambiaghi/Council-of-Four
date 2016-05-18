08c08290c0dc873ca67c9dac58c7febfef976ff9
ale_dichio <alessio.dichio@mail.polimi.it>
2016-05-18 11:48:35 +0200

    Ho creato la classe Colors e testato il metodo GetRandomColor

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/Colors.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/Colors.java
@@ -1,5 +1,27 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work.field;
+import java.util.Random;
 
-public class Colors {
-
+public enum Colors {
+	
+	White("255, 255, 255"),
+	Black("0, 0, 0"), 
+	Cyan ("0, 128, 255"), 
+	Orange ("255, 128, 0"), 
+	Pink("255, 153, 255"), 
+	Purple("88, 86, 214");
+	
+	public String rgb;
+		 
+	private Colors (final String rgb){
+		this.rgb=rgb;		
+	}
+	public String getRGB (){
+		return rgb;
+	}
+	
+	// the method getRandomColor select a Color from the enum list 
+	public static Colors getRandomColor(){
+		Random random = new Random ();
+		return values()[random.nextInt(values().length)];
+	}
 }
