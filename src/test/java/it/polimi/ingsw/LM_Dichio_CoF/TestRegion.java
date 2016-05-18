83346282aba115a9f1d8dcd701248b72c60d5dc4
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-18 18:06:22 +0200

    Test funzionante per verificare che la regione assegni correttamente i nomi alle sue città

---
+++ src/test/java/it/polimi/ingsw/LM_Dichio_CoF/TestRegion.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.LM_Dichio_CoF;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.Region;
+import junit.framework.Assert;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.City;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.NameCity;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.NameRegion;
+
+public class TestRegion {
+	
+	@Test
+	public void test(){
+		
+		Region region = new Region(8, NameRegion.Mountain,16);
+		
+		
+		City[] arrayCity = new City[7];
+		
+		arrayCity = region.getArrayCity();
+		
+		for (City city : arrayCity){
+			System.out.println(city.getName());
+		}
+		
+	}
+	
+	
+	
+}
