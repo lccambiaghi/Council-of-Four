5bb3663ec80bded03af6be200873ba220df54d1d
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-18 15:48:56 +0200

    Ho modificato completamente, ora Connection presenta il ciclo infinito
    per gestire le connessioni. FINORA solo quella di tipo socket

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/Connection.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/Connection.java
@@ -1,69 +1,27 @@
 package it.polimi.ingsw.LM_Dichio_CoF.connection;
 
-import it.polimi.ingsw.LM_Dichio_CoF.work.Player;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-import java.util.Scanner;
 
-import it.polimi.ingsw.LM_Dichio_CoF.work.GameSide;
+import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
 
-public class Connection extends Thread{
-
-	char typeOfConnection;
-	String IP;
+public class Connection{
 	
-	//DI PROVA PER IL TEST
-	Scanner input = new Scanner(System.in);
+	private ServerSocket serverSocket;
 	
-	public Connection(){
+	public Connection() throws IOException{
 		
-	/*	
-	 * 	ATTENDE connessione con un client (RMI o SOCKET)
-	 * 
-	 * 
-	 *	Connessione stabilita con un client
-	 *
-	 * 	MEMORIZZA L'IP DEL CLIENT NELLA VARIABILE "IP"
-	 * 	MEMORIZZA ANCHE IL TIPO DI CONNESSIONE
-	 */
+		serverSocket = new ServerSocket(Constant.PORT); 
+		while(true){
+			Socket clientSocket = serverSocket.accept();
+			new SocketConnectionWithClient(clientSocket);
+			
+		}
 		
-			//SEMPRE DI PROVA PER IL TEST
-			String s = input.nextLine(); 
-			typeOfConnection=s.charAt(0);
-			
-			
-			
-			IP="127.0.0.1";
-			
-			
-	
-	/*
-	 * 	CREA UN THREAD PER GESTIRE LA CONNESSIONE
-	 */
-		
-			start();
 	
 	}
 	
-	public void run(){
-		
-		Player player = new Player(typeOfConnection,IP); 
-		GameSide.handlePlayer(player);
-		
-	}
+	
 }
-
-/*
- * 	Salvataggio indirizzo IP come parametro nella classe Giocatore
- * 
- * 	Passa al package work, classe GameSide 
- * 
- * 
- */
-
-//Attesa scelta CLI o GUI
-
-			// Accettazione richiesta -> salvataggio scelta parametro nella classe Giocatore
-			
-			// Attesa username 
-			
-			// Accettazione username -> salvataggio parametro nella classe Giocatore
