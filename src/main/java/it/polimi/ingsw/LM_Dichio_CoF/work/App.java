0b117f7968ca4fcc8f05f34c59e4c48006da6ed5
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-18 15:49:14 +0200

    Nulla di che, come prima, lancia il lato gioco

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/App.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/App.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work;
 
+import java.io.IOException;
+
 public class App 
 {
     private static GameSide gameSide;
 
-	public static void main( String[] args )
+	public static void main( String[] args ) throws IOException
     {
         gameSide = new GameSide();
     }
