e32c770fe7e28f1299c0a02f486d738e020fd42b
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-18 15:48:14 +0200

    La classe PlayerSide ora ha un MAIN, quindi attenzione a dove fare run,
    perchè adesso si può far partire anche questa classe. Inoltre ho
    aggiunto il socket in localhost e porta fissa 3000

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/PlayerSide.java
@@ -0,0 +1,45 @@
+package it.polimi.ingsw.LM_Dichio_CoF_PlayerSide;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class PlayerSide {
+
+	private  Socket mySocket;
+	private final static String address="localhost";
+	
+	public PlayerSide() {
+		
+		System.out.println("I am alive");	
+		
+		connectToServer();
+	
+	}
+	
+	
+	private void connectToServer(){
+		
+		try {
+			
+			mySocket = new Socket(address,3000);
+			
+			System.out.println("I am connected to the Server");
+			
+			
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public static void main (String[] args){
+		
+		new PlayerSide();
+		
+	}
+		
+	
+}
