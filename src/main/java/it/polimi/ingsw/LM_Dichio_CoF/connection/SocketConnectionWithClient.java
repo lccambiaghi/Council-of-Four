0c98eb9419bcf008ca4184221f2083a05d6e4314
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-18 15:52:07 +0200

    Un'intera classe di tipo Thread che gestisca la connessione con un client

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/SocketConnectionWithClient.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.LM_Dichio_CoF.connection;
+
+import java.net.Socket;
+
+import it.polimi.ingsw.LM_Dichio_CoF.work.GameSide;
+import it.polimi.ingsw.LM_Dichio_CoF.work.Player;
+
+public class SocketConnectionWithClient extends Thread {
+
+	Socket clientSocket;
+	Player player;
+	
+	SocketConnectionWithClient(Socket clientSocket){
+		
+		this.clientSocket=clientSocket;
+		
+		player = new Player("s");
+		
+		player.setPlayerSocket(clientSocket);
+		
+		start();
+		
+	}
+	
+	
+	public void run(){
+		
+		GameSide.handlePlayer(player);
+		
+	}
+}
