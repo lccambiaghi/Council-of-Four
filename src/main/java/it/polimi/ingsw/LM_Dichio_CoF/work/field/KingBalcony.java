aa38134bb919044bcafbba6c18c1fa70c3bc97b4
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-20 20:12:14 +0200

    KingBalcony è per ora sola sottoclasse di balcony

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/KingBalcony.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/KingBalcony.java
@@ -2,6 +2,14 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
 public class KingBalcony extends Balcony {
 
+
 	King king;
+	AvailableCouncilor availableCouncilor;
+	
+	public KingBalcony(AvailableCouncilor availableCouncilor, String nameBalcony) {
+		super(availableCouncilor, nameBalcony);	
+	}
+	
+	
 	
 }
---
+++ src/test/java/it/polimi/ingsw/LM_Dichio_CoF/TestKingBalcony.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.LM_Dichio_CoF;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.AvailableCouncilor;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.Balcony;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.Councilor;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.KingBalcony;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.NameRegion;;
+
+public class TestKingBalcony {
+
+	@Test
+	public void test() {
+		
+		AvailableCouncilor availableCouncilor = new AvailableCouncilor();
+		
+		// Polymorphism
+		Balcony kingBalcony = new KingBalcony (availableCouncilor,"KingBalcony");
+		
+		ArrayList<Councilor> arrayListCouncilor = kingBalcony.getArrayListCouncilor();
+		
+		System.out.println(kingBalcony.getNameBalcony());
+		for(Councilor c: arrayListCouncilor){
+			System.out.println(c.getColor());
+		}
+		
+		assertEquals(4, arrayListCouncilor.size());
+		
+		
+	}
+	
+}
