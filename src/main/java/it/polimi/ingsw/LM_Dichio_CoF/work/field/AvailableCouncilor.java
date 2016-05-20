97a84bfc013021a97b8339bb099bf2286940e086
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-20 20:09:17 +0200

    AvailableCouncilor ed il relativo test.
    La classe contiene inizialmente tutti i consiglieri (il costruttore di
    essa viene lanciato solo una volta, nella classe Field) che poi verranno
    tolti dalle varie balconate attraverso la remove.
    La classe prevede anche di creare un ordine casuale dei consiglieri.

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/AvailableCouncilor.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/AvailableCouncilor.java
@@ -1,7 +1,67 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.Color;
+
 public class AvailableCouncilor {
 
-	Councilor councilor[];
+	/*
+	 * Singleton DESIGN PATTERN
+	 * The constructor is private, so that it is inaccessible from outside,
+	 * Once the class is created for the first time, the method "createList()" is invoked
+	 */
+	
+	ArrayList <Councilor> arrayListCouncilor;
+	
+	
+	/// NON POSSO METTERLO SINGLETON :( o si? :D
+	/*
+	private static AvailableCouncilor instance = null; 
+	private AvailableCouncilor() { } 
+	public static AvailableCouncilor getInstance() {
+		if (instance == null) {
+			instance = new AvailableCouncilor();
+			createList();
+		}
+		return instance;
+	}
+	*/
+	
+	public AvailableCouncilor(){
+		arrayListCouncilor = new ArrayList<Councilor>();
+		createArrayList();
+	}
+	
+	
+	/*
+	 * This method creates the councilers, put them in order in the arrayList "listCouncilor" and then
+	 * shuffle it to have them randomly
+	 */
+	private void createArrayList(){
+		
+		int councilorsPerColor = Constant.TOTAL_NUMBER_COUNCILORS/Constant.NUMBER_OF_COLORS;
+		
+		for(int i=0; i< Constant.TOTAL_NUMBER_COUNCILORS; i++){
+			int indexOfColor =(int)(i/councilorsPerColor);
+			Councilor councilor = new Councilor(Color.getColorFromIndex(indexOfColor));
+			arrayListCouncilor.add(councilor);
+		}
+			
+		Collections.shuffle(arrayListCouncilor);
+		
+	}
+	
+	public ArrayList<Councilor> getArrayListCouncilor() {
+		return arrayListCouncilor;
+	}
+	
+	public Councilor removeCouncilor(){
+		Councilor councilor = arrayListCouncilor.remove(0);
+		return councilor;
+	}
+	
 	
 }
---
+++ src/test/java/it/polimi/ingsw/LM_Dichio_CoF/TestAvailableCouncilor.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.LM_Dichio_CoF;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.*;
+
+public class TestAvailableCouncilor {
+
+	@Test
+	public void test() {
+		/*
+		 * This test controls the correct implementation of the singleton and the 
+		 * list of councilers that are created the first time
+		 */
+		AvailableCouncilor availableCouncilor = new AvailableCouncilor();
+		
+		ArrayList<Councilor> a = availableCouncilor.getArrayListCouncilor();
+		
+		for(Councilor c: a){
+			System.out.println(c.getColor());
+		}
+	
+		assertEquals(24, a.size());
+	}
+
+}
