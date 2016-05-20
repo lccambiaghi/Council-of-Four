c6bc8f8d393fb5605617cac221c2c2862cb02c09
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-20 20:07:19 +0200

    Constant prevede più costanti, sconsiglio infatti l'uso dei numeri nei cicli e quant'altro, poichè attraverso le costanti si può modificare tutto "dall'alto"

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Constant.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Constant.java
@@ -2,6 +2,11 @@ package it.polimi.ingsw.LM_Dichio_CoF.work;
 
 public class Constant {
 	
-	public static final int PORT=3000;
+	public static final int PORT_SOCKET=3000;
+	public static final int NUMBER_COUNCILERS_PER_BALCONY=4;
+	public static final int NUMBER_OF_COLORS=6; // COUNCILORS
+	public static final int TOTAL_NUMBER_COUNCILORS=24; //IT HAS TO BE A MULTIPLE OF THREE
+	public static final int NUMBER_INITIAL_POLITIC_CARDS=6;
+	
 	
 }
