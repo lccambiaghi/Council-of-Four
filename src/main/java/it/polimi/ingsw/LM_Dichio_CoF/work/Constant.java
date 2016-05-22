d2c09631e668091a7ee537f86ae777f5e616c9bd
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 22:28:21 +0200

    Aggiunta la variabile INITIAL_KING_CITY

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Constant.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Constant.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work;
 
+import it.polimi.ingsw.LM_Dichio_CoF.work.field.NameCity;
+
 public class Constant {
 	
 	public static final int PORT_SOCKET=3000;
@@ -10,11 +12,14 @@ public class Constant {
 	public static final int INITIAL_RICHNESS_FIRST_PLAYER=10;
 	public static final int NUMBER_OF_REGIONS=3;
 	public static final int NUMBER_FACE_UP_PERMIT_CARD_PER_REGION=2;
+	public static final int NUMBER_BONUS_PER_PERMIT_CARD=2;
 	public static final int MAX_CITIES_PER_PERMIT_CARD=3;
 	public static final int MIN_CITIES_PER_PERMIT_CARD=1;
 	public static final int MIN_CITIES_PER_REGION=5;
 	public static final int MAX_RICHNESS=20;
 	public static final int MAX_NOBILITY=20;
 	public static final int MAX_VICTORY=100;
+	
+	public static final NameCity INITIAL_KING_CITY= NameCity.J;
 	
 }
