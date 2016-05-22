18fd86f261225d940d4b7ecbc8613a44eb20e26f
Luca Cambiaghi <luca.cambiaghi@mail.polimi.it>
2016-05-22 11:29:57 +0200

    Aggiornate le costanti

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Constant.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Constant.java
@@ -7,6 +7,9 @@ public class Constant {
 	public static final int NUMBER_OF_COLORS=6; // COUNCILORS
 	public static final int TOTAL_NUMBER_COUNCILORS=24; //IT HAS TO BE A MULTIPLE OF THREE
 	public static final int NUMBER_INITIAL_POLITIC_CARDS=6;
-	
+	public static final int INITIAL_RICHNESS_FIRST_PLAYER=10;
+	public static final int MAX_RICHNESS=20;
+	public static final int MAX_NOBILITY=20;
+	public static final int MAX_VICTORY=100;
 	
 }
