7a32cea542f8173701c3b69410f4be536b5a7b29
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 22:36:16 +0200

    Cambiato gestione bonus

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/PermitCard.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/PermitCard.java
@@ -8,7 +8,9 @@ import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
 
 public class PermitCard {
 
+	City[] possibleCityWhereBuild;
 	City[] arrayCityWhereBuild;
+	Bonus[] arrayBonus;
 	
 	/*
 	 * Constructor
@@ -18,28 +20,38 @@ public class PermitCard {
 	 */
 	public PermitCard(City[] possibleCityWhereBuild) {
 		
+		this.possibleCityWhereBuild = new City[possibleCityWhereBuild.length];
+		
 		Random random = new Random();
 		
 		int numberPossibleCities=Constant.MAX_CITIES_PER_PERMIT_CARD +
-				possibleCityWhereBuild.length - Constant.MIN_CITIES_PER_REGION;
+				this.possibleCityWhereBuild.length - Constant.MIN_CITIES_PER_REGION;
 		
 		int lengthCityWhereBuild = 
 				random.nextInt(numberPossibleCities-Constant.MIN_CITIES_PER_PERMIT_CARD+1)+
 				Constant.MIN_CITIES_PER_PERMIT_CARD;
 		
-		Collections.shuffle(Arrays.asList(possibleCityWhereBuild));
+		Collections.shuffle(Arrays.asList(this.possibleCityWhereBuild));
 		
 		arrayCityWhereBuild = new City[lengthCityWhereBuild];
 		
 		for(int i=0; i< lengthCityWhereBuild; i++){
-			arrayCityWhereBuild[i]=possibleCityWhereBuild[i];
+			arrayCityWhereBuild[i]=this.possibleCityWhereBuild[i];
+		}
+		
+		arrayBonus = new Bonus[Constant.NUMBER_BONUS_PER_PERMIT_CARD];
+		for(int i=0; i < arrayBonus.length; i++){
+			arrayBonus[i] = new BonusPermitCard();
 		}
 		
 	}
-
+	
 	public City[] getArrayCityWhereBuild() {
 		return arrayCityWhereBuild;
-	}	
-	
+	}
+
+	public Bonus[] getArrayBonus() {
+		return arrayBonus;
+	}		
 	
 }
