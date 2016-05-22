c000d9e772344841158faeab814a3e36069ea8eb
Luca Cambiaghi <luca.cambiaghi@mail.polimi.it>
2016-05-22 16:37:41 +0200

    Creata classe BonusName

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/BonusName.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.LM_Dichio_CoF.work.field;
+
+/**
+ * Created by Luca on 22/05/16.
+ */
+public enum BonusName {
+
+    Assistant(3),
+    Richness(5),
+    Nobility(2),
+    Victory(5),
+    Cards(3),
+    MainMove(1);
+
+    private int maxIncrement;
+
+    BonusName(int maxIncrement) {this.maxIncrement=maxIncrement;}
+
+    /*
+    public static BonusName getBonusName (int index){return values()[index];}
+     */
+
+    public static int getMaxIncrement(BonusName bonusName){return bonusName.maxIncrement;}
+}
