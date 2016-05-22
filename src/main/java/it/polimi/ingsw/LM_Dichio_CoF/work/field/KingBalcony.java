80e5060bd1fcaa9af36bbe996888490be878d70e
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 22:32:44 +0200

    Non inizializza più il re, il quale è gestito dal "Field"

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/KingBalcony.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/KingBalcony.java
@@ -2,14 +2,11 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
 public class KingBalcony extends Balcony {
 
-
-	King king;
 	AvailableCouncilor availableCouncilor;
 	
 	public KingBalcony(AvailableCouncilor availableCouncilor, String nameBalcony) {
 		super(availableCouncilor, nameBalcony);	
 	}
-	
 	
 	
 }
