ab662e056880c98ac7184f7a0997d485f79be19b
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 22:33:01 +0200

    Ho solo aggiunto un commento

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NameCity.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NameCity.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
 public enum NameCity {
 	
+	// Constant.INITIAL_KING_CITY is a city of this enum
+	
 	A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y;
 	
 	public static NameCity getNameCity (int index){
