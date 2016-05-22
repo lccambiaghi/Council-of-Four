b36049247a02adb0f597a478f5172b05cc2e83a1
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 18:46:15 +0200

    Ho aggiunto il minimo e massimo numero di bonus per regione
    ed i relativi metodi getter

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Configurations.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Configurations.java
@@ -3,15 +3,33 @@ package it.polimi.ingsw.LM_Dichio_CoF.work;
 public class Configurations {
 
 	int numberCities;
+	int minNumberBonusPerCity;
+	int maxNumberBonusPerCity;
 	
-	Configurations(int numberCities){
+	public Configurations(int numberCities, int minNumberBonusPerCity,
+			int maxNumberBonusPerCity){
 		
 		this.numberCities=numberCities;
+		this.minNumberBonusPerCity=minNumberBonusPerCity;
+		this.maxNumberBonusPerCity=maxNumberBonusPerCity;
 		
+	}
+	
+	
+	public int getMinNumberBonusPerCity() {
+		return minNumberBonusPerCity;
+	}
 
-		
-		
-		
+	public void setMinNumberBonusPerCity(int minNumberBonusPerCity) {
+		this.minNumberBonusPerCity = minNumberBonusPerCity;
+	}
+
+	public int getMaxNumberBonusPerCity() {
+		return maxNumberBonusPerCity;
+	}
+
+	public void setMaxNumberBonusPerCity(int maxNumberBonusPerCity) {
+		this.maxNumberBonusPerCity = maxNumberBonusPerCity;
 	}
 
 	public int getNumberCities() {
