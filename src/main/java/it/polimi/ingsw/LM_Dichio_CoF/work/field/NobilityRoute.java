7ab1ed9f62e8c6a74a37b5e9a07872f95ee705fc
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 11:36:15 +0200

    Ho rinominato NoblityRoute in NobilityRoute.
    Scritto il cotruttore, i metodi movePlayer e getPosition

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRoute.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.LM_Dichio_CoF.work.field;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
+import it.polimi.ingsw.LM_Dichio_CoF.work.Player;
+
+public class NobilityRoute implements Route{
+
+	RouteCell nobilityRouteCell;
+	Map <Player,Integer> nobilityMap = new HashMap<>();
+	
+	public NobilityRoute(ArrayList<Player> arrayListPlayer){
+		
+		for(int i=0; i<arrayListPlayer.size(); i++){
+			nobilityMap.put(arrayListPlayer.get(i), 0);	
+		}
+		
+	}
+	
+	public void movePlayer(int increment, Player player){
+		int oldValue = nobilityMap.get(player);
+		if (oldValue+increment < Constant.MAX_NOBILITY)
+			nobilityMap.replace(player, oldValue+increment);
+		else
+			nobilityMap.replace(player, Constant.MAX_NOBILITY);
+	}
+	
+	public int getPosition(Player player){
+		return nobilityMap.get(player);
+	}
+	
+	
+}
