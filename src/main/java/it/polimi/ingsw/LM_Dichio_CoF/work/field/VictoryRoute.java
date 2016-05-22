5a57ec1b3597301bab17983bc4e4a5f32254f434
ale_dichio <alessio.dichio@mail.polimi.it>
2016-05-22 11:37:19 +0200

    Modificata la classe VictoryRoute, implementati i metodi dell'interfaccia Route.

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/VictoryRoute.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/VictoryRoute.java
@@ -1,7 +1,32 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
+import it.polimi.ingsw.LM_Dichio_CoF.work.Player;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
+
 public class VictoryRoute implements Route {
 
+	Map <Player, Integer> victoryMap = new HashMap <>();
 	
+	public VictoryRoute (ArrayList <Player> arrayListPlayer){
+		for(int i=0; i<arrayListPlayer.size(); i++)
+			victoryMap.put(arrayListPlayer.get(i), 0);
+		}
+		
+	public void movePlayer (int increment, Player player){
+		int oldValue = victoryMap.get(player);
+		if ((oldValue+increment)<Constant.MAX_VICTORY)
+			victoryMap.replace(player, oldValue+increment);
+		else 
+			victoryMap.replace(player, Constant.MAX_VICTORY);
+		}
+	
+	public int getPosition (Player player){
+		int victoryPosition;
+		victoryPosition=victoryMap.get(player);
+		return victoryPosition;
+	}
 	
 }
