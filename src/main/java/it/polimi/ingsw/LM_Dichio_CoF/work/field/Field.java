649088059e950120ba72880a70084bb64e728a82
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 22:35:05 +0200

    Ho messo in commento la arte di inizializzazione balconate, dà qualche problema..

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/Field.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/Field.java
@@ -2,15 +2,17 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
 import java.util.ArrayList;
 
+
 import it.polimi.ingsw.LM_Dichio_CoF.work.*;
 
 public class Field {
 
-	Region arrayRegion[];
-	Balcony arrayBalcony[];
+	Region[] arrayRegion;
+	City[] arrayCity;
+	Balcony[] arrayBalcony;
 	AvailableCouncilor availableCouncilor;
 	Route route;
-	
+	King king;
 	
 	public Field(Configurations config, ArrayList<Player> arrayListPlayer) {
 		
@@ -19,10 +21,36 @@ public class Field {
 		 * inoltre bisogna trovare un modo (matrice di inceidenza o liste) per creare le città vicine
 		 */
 		
-		arrayRegion[0] = new Region (config,NameRegion.Sea);
+		arrayRegion = new Region[Constant.NUMBER_OF_REGIONS];
+		
+		arrayRegion[0] = new Region (config, NameRegion.Sea);
 		arrayRegion[1] = new Region (config, NameRegion.Hill);
 		arrayRegion[2] = new Region (config, NameRegion.Mountain);
+		
+		/*
+		 * This array will contain the cities created by every region
+		 */
+		arrayCity = new City[config.getNumberCities()];
+		
+		/*
+		 * k=iterator in arrayCity
+		 * j=iterator in arrayRegion
+		 * i=iterator in arrayCity(of every region)
+		 */
+		for(int k=0,j=0; j<Constant.NUMBER_OF_REGIONS; j++){
+			for(int i=0; i<arrayRegion[j].getArrayCity().length; i++, k++){
+				arrayCity[k]=(arrayRegion[j].getArrayCity())[i];
+				
+				/*
+				 * if the City is the king one it initializes the king
+				 */
+				if(arrayCity[k].getNameCity() == Constant.INITIAL_KING_CITY){
+					king = new King(arrayCity[k]);
+				}
+			}
+		}
 
+		
 		
 		
 		//GESTIONE BALCONATE
@@ -31,18 +59,24 @@ public class Field {
 		/*
 		 * Create one (AND ONLY FOR ALL THE GAME) instance for the Available Councilor
 		 */
+		
+		/*
 		availableCouncilor = new AvailableCouncilor();
 		
 		arrayBalcony[0] = new Balcony (availableCouncilor, NameRegion.Sea.toString()+"Balcony");
 		arrayBalcony[1] = new Balcony (availableCouncilor, NameRegion.Hill.toString()+"Balcony");
 		arrayBalcony[2] = new Balcony (availableCouncilor, NameRegion.Mountain.toString()+"Balcony");
 		arrayBalcony[3] = new KingBalcony (availableCouncilor, "KingBalcony");
-		
+		*/
 		
 		
 		//GESTIONE PERCORSI
 		
 		
+	}
+	
+	public City[] getArrayCity(){
+		return arrayCity;
 	}
 	
 }
