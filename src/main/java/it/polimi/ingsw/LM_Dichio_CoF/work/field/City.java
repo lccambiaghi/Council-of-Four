c6eb6afb71363c83a03dd537a68124c42d703d3a
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-22 22:34:19 +0200

    Piccole modifiche

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/City.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/City.java
@@ -3,33 +3,44 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 import java.util.Random;
 
 import it.polimi.ingsw.LM_Dichio_CoF.work.Configurations;
+import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
 
 public class City {
 
 	NameCity nameCity;
+	NameRegion nameRegion;
 	int numberBonus;
 	Bonus[] arrayBonus;
 	City nearbyCity[];
 	
 	
 	/*
-	 * As we can see the Constructor only receives the name of the city given by the Region
+	 * The constructor receives the configurations, the name of the city given by the Region
+	 * and the name of the region itself
 	 */
-	public City(Configurations config, NameCity nameCity){
+	public City(Configurations config, NameCity nameCity, NameRegion nameRegion){
+		
 		Random random = new Random();
 		
 		this.nameCity=nameCity;
+		this.nameRegion=nameRegion;
 		
-		numberBonus= random.nextInt(config.getMaxNumberBonusPerCity()-
-				config.getMinNumberBonusPerCity()+1) +
-				config.getMinNumberBonusPerCity();
-		
-		arrayBonus = new Bonus[numberBonus];
-		for(int i=0; i< numberBonus; i++){
-			// Polymorphism
-			arrayBonus[i]= new BonusCity();
-		}
-		
+		/* 
+		 * This "if" refers to the city of the king: it doesn't have to have bonuses
+		 */
+		if(!nameCity.equals(Constant.INITIAL_KING_CITY)){
+			numberBonus= random.nextInt(config.getMaxNumberBonusPerCity()-
+					config.getMinNumberBonusPerCity()+1) +
+					config.getMinNumberBonusPerCity();
+			
+			if(numberBonus!=0){
+				arrayBonus = new Bonus[numberBonus];
+				for(int i=0; i< numberBonus; i++){
+					// Polymorphism
+					arrayBonus[i]= new BonusCity();
+				}
+			}
+		}	
 	}	
 	
 	public NameCity getNameCity() {
@@ -40,9 +51,26 @@ public class City {
 		this.nameCity = nameCity;
 	}
 	
+	public NameRegion getNameRegion() {
+		return nameRegion;
+	}
+
+	public void setNameRegion(NameRegion nameRegion) {
+		this.nameRegion = nameRegion;
+	}
+
 	public Bonus[] getArrayBonus() {
 		return arrayBonus;
 	}
 	
+	
+	/*
+	 * This methods tells if the current city has bonus or not (if it is null)
+	 */
+	public boolean hasBonus(){
+		if (arrayBonus== null)
+			return false;
+		return true;
+	}	
 	
 }
