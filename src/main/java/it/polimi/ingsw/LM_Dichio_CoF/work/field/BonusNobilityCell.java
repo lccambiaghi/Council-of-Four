b3886f974b2d1aa3e499aa5ef1edd922bae306d0
ale_dichio <alessio.dichio@mail.polimi.it>
2016-05-24 18:29:01 +0200

    Creata la classe Bonus Nobility Cell come sottoclasse di Bonus

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/BonusNobilityCell.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.LM_Dichio_CoF.work.field;
+
+public class BonusNobilityCell extends Bonus {
+
+	public BonusNobilityCell (BonusName bonusName, int increment){
+		this.bonusName=bonusName;
+		this.increment=increment;
+		
+	}
+}
