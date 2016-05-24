32b1456c9391884bba5113ef497896291984a6ea
ale_dichio <alessio.dichio@mail.polimi.it>
2016-05-24 18:31:19 +0200

    Creato il costruttore e assegnati con uno switch tutti i valori dei bonus presenti nel gioco base alle rispettive caselle.

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
@@ -2,6 +2,66 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
 public class NobilityRouteCell {
 	
-	Bonus bonus[];
+	Bonus [] bonus;
+	boolean hasBonus;
+	int i;
+	
+	public NobilityRouteCell(int index){
+		this.i=index;
+		
+		switch (i){
+		case 2:
+			bonus = new Bonus [2];
+			bonus[0] = new BonusNobilityCell(BonusName.Richness, 2);
+			bonus[1] = new BonusNobilityCell(BonusName.Victory, 2);
+			break;
+		case 4:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.BuiltCityBonus, 1);
+			break;
+		case 6:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.MainMove, 1);
+			break;
+		case 8:
+			bonus = new Bonus [2];
+			bonus[0] = new BonusNobilityCell(BonusName.Victory, 3);
+			bonus[1] = new BonusNobilityCell(BonusName.Cards, 1);
+			break;
+		case 10:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.FaceUpPermitCard, 1);
+			break;
+		case 12:
+			bonus = new Bonus [2];
+			bonus[0] = new BonusNobilityCell(BonusName.Victory, 5);
+			bonus[1] = new BonusNobilityCell(BonusName.Assistant, 1);
+			break;
+		case 14:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.OwnedPermitCardBonus, 1);
+			break;
+		case 16:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.TwoBuiltDifferentBonus, 1);
+			break;
+		case 18:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.Victory, 8);
+			break;
+		case 19:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.Victory, 2);
+			break;
+		case 20:
+			bonus = new Bonus [1];
+			bonus[0] = new BonusNobilityCell(BonusName.Victory, 3);
+			break;
+		default:
+			bonus = null;
+		}
+
+		
+	}
 
 }
