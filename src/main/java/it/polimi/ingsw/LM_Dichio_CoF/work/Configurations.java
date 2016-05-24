546a6897f7af218c536ebc7f7565761e7753b0ae
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-24 14:52:22 +0200

    Aggiunto il carattere di difficoltà

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Configurations.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Configurations.java
@@ -3,19 +3,31 @@ package it.polimi.ingsw.LM_Dichio_CoF.work;
 public class Configurations {
 
 	int numberCities;
+	char difficulty;
+	
 	int minNumberBonusPerCity;
 	int maxNumberBonusPerCity;
 	
-	public Configurations(int numberCities, int minNumberBonusPerCity,
+	public Configurations(int numberCities, char difficulty, int minNumberBonusPerCity,
 			int maxNumberBonusPerCity){
 		
 		this.numberCities=numberCities;
+		this.difficulty=difficulty;
 		this.minNumberBonusPerCity=minNumberBonusPerCity;
 		this.maxNumberBonusPerCity=maxNumberBonusPerCity;
 		
 	}
 	
 	
+	public char getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(char difficulty) {
+		this.difficulty = difficulty;
+	}
+
+
 	public int getMinNumberBonusPerCity() {
 		return minNumberBonusPerCity;
 	}
