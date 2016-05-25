cba2ef619ccab60e17198c1c330949631c4ee4d2
ale_dichio <alessio.dichio@mail.polimi.it>
2016-05-25 10:06:18 +0200

    Inseriti i metodi hasBonus e getArrayBonus

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
@@ -2,8 +2,7 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
 public class NobilityRouteCell {
 	
-	Bonus [] bonus;
-	boolean hasBonus;
+	Bonus [] arrayBonus;
 	int i;
 	
 	public NobilityRouteCell(int index){
@@ -11,57 +10,66 @@ public class NobilityRouteCell {
 		
 		switch (i){
 		case 2:
-			bonus = new Bonus [2];
-			bonus[0] = new BonusNobilityCell(BonusName.Richness, 2);
-			bonus[1] = new BonusNobilityCell(BonusName.Victory, 2);
+			arrayBonus = new Bonus [2];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.Richness, 2);
+			arrayBonus[1] = new BonusNobilityCell(BonusName.Victory, 2);
 			break;
 		case 4:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.BuiltCityBonus, 1);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.BuiltCityBonus, 1);
 			break;
 		case 6:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.MainMove, 1);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.MainMove, 1);
 			break;
 		case 8:
-			bonus = new Bonus [2];
-			bonus[0] = new BonusNobilityCell(BonusName.Victory, 3);
-			bonus[1] = new BonusNobilityCell(BonusName.Cards, 1);
+			arrayBonus = new Bonus [2];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.Victory, 3);
+			arrayBonus[1] = new BonusNobilityCell(BonusName.Cards, 1);
 			break;
 		case 10:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.FaceUpPermitCard, 1);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.FaceUpPermitCard, 1);
 			break;
 		case 12:
-			bonus = new Bonus [2];
-			bonus[0] = new BonusNobilityCell(BonusName.Victory, 5);
-			bonus[1] = new BonusNobilityCell(BonusName.Assistant, 1);
+			arrayBonus = new Bonus [2];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.Victory, 5);
+			arrayBonus[1] = new BonusNobilityCell(BonusName.Assistant, 1);
 			break;
 		case 14:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.OwnedPermitCardBonus, 1);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.OwnedPermitCardBonus, 1);
 			break;
 		case 16:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.TwoBuiltDifferentBonus, 1);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.TwoBuiltDifferentBonus, 1);
 			break;
 		case 18:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.Victory, 8);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.Victory, 8);
 			break;
 		case 19:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.Victory, 2);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.Victory, 2);
 			break;
 		case 20:
-			bonus = new Bonus [1];
-			bonus[0] = new BonusNobilityCell(BonusName.Victory, 3);
+			arrayBonus = new Bonus [1];
+			arrayBonus[0] = new BonusNobilityCell(BonusName.Victory, 3);
 			break;
 		default:
-			bonus = null;
+			arrayBonus = null;
 		}
 
-		
 	}
 
+	public boolean hasBonus(){
+		if (arrayBonus== null)
+			return false;
+		return true;
+	}
+	
+	public Bonus[] getArrayBonus() {
+		return arrayBonus;
+	}
+	
 }
