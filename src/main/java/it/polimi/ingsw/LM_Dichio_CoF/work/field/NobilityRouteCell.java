cc7880dd127dd62ac00fb51bd2b4d126a65aa462
ale_dichio <alessio.dichio@mail.polimi.it>
2016-05-29 11:35:40 +0200

    Sistemato il costruttore, tuttavia bisogna trovare il modo di incrementare l'indice esterno dell'array. ora i bonus sono sempre uguali

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
@@ -5,15 +5,16 @@ import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
 public class NobilityRouteCell {
 	
 	private NobilityCellBonus [] arrayBonus;
-	int i;
 
 	/* The constructor creates the NobilityRouteCell assigning it bonus
 	   according to its index, specified as parameter*/
 	public NobilityRouteCell(int index){
-				
+
 		if(((index%2==0)&&(index!=0)) || (index==19)){
-			for(int j=0; j< Constant.NOBILITY_CELL_BONUSES.length; j++)
-			arrayBonus = Constant.NOBILITY_CELL_BONUSES [j];
+			for(int j=0; j< Constant.NOBILITY_CELL_BONUSES[j].length; j++){
+				arrayBonus = Constant.NOBILITY_CELL_BONUSES[j];
+			}
+			
 		}
 		else 
 			arrayBonus=null;
