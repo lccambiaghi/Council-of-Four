880388208f04a66af7ecc5d0dff9e3ff50e21d0d
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-29 20:09:55 +0200

    Il nuovo costruttore non fa il controllo sulle costanti in base all'indice, ma riceve direttamente l'array di bonus

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/NobilityRouteCell.java
@@ -1,30 +1,23 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
-import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
-
 public class NobilityRouteCell {
 	
-	private NobilityCellBonus [] arrayBonus;
+	int index;
+	private Bonus [] arrayBonus;
 
-	/* The constructor creates the NobilityRouteCell assigning it bonus
-	   according to its index, specified as parameter*/
-	public NobilityRouteCell(int index){
+	/* The constructor creates the NobilityRouteCell, assigning it index and
+	   the array of bonuses both passed as parameters */
+	public NobilityRouteCell(int index, Bonus[] arrayBonus){
 
-		if(((index%2==0)&&(index!=0)) || (index==19)){
-			for(int j=0; j< Constant.NOBILITY_CELL_BONUSES[j].length; j++){
-				arrayBonus = Constant.NOBILITY_CELL_BONUSES[j];
-			}
-			
-		}
-		else 
-			arrayBonus=null;
+		this.index=index;
+		this.arrayBonus=arrayBonus;
 		
 	}
 
 	/* This method checks if a cell has bonus*/
 	public boolean hasBonus(){ return arrayBonus != null; }
 	
-	public NobilityCellBonus [] getArrayBonus() {
+	public Bonus [] getArrayBonus() {
 		return arrayBonus;
 	}
 	
