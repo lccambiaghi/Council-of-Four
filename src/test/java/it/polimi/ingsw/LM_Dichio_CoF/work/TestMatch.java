b706fdc5340ad8fba296b060b5b43396cb20a995
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-31 00:07:02 +0200

    Creato il test del match per verificare le carte iniziali

---
+++ src/test/java/it/polimi/ingsw/LM_Dichio_CoF/work/TestMatch.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.LM_Dichio_CoF.work;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.jar.Attributes.Name;
+
+import org.junit.Test;
+
+import it.polimi.ingsw.LM_Dichio_CoF.TestCases;
+
+public class TestMatch {
+
+	@Test
+	public void giveInitialCards() {
+		
+		TestCases testCases = new TestCases();
+		
+		ArrayList <Player> arrayListPlayer = testCases.arrayListPlayer();
+	
+		Match match = new Match(arrayListPlayer);
+		
+		for(int itPlayer=0; itPlayer<arrayListPlayer.size(); itPlayer++){
+			ArrayList<PoliticCard> arrayListPoliticCard = arrayListPlayer.get(itPlayer).getArrayListPoliticCard();
+			for(int itCard=0; itCard<arrayListPoliticCard.size(); itCard++){
+				System.out.println(arrayListPoliticCard.get(itCard).getCardColor().toString());
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
