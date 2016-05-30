350e598a21f65fd2f73bea479512765f9ca2dec0
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-30 23:00:03 +0200

    Adesso il player può creare un file configurations, anche se per ora l'assegnazione dei parametri è fissa e hard coded

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/Configurations.java
@@ -0,0 +1,133 @@
+package it.polimi.ingsw.LM_Dichio_CoF_PlayerSide;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Configurations implements Serializable{
+
+	private int citiesNumber;
+
+	private int permitCardBonusNumberMin;
+	private int permitCardBonusNumberMax;
+	
+	boolean nobilityBonusRandom;
+
+	int nobilityBonusNumber; //how many bonus the user wants on the nobilityRoute
+
+	boolean cityLinksPreconfigured;
+	private char difficulty; // e, n, h
+	int cityLinksMatrix[][];
+
+	boolean cityBonusRandom;
+	private int cityBonusNumberMin;
+	private int cityBonusNumberMax;
+	HashMap<String,Integer> cityBonusArrayMaps[];
+
+	public Configurations(){}
+
+	public char getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(char difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public int getCityBonusNumberMin() {
+		return cityBonusNumberMin;
+	}
+
+	public void setCityBonusNumberMin(int cityBonusNumberMin) {
+		this.cityBonusNumberMin = cityBonusNumberMin;
+	}
+
+	public int getCityBonusNumberMax() {
+		return cityBonusNumberMax;
+	}
+
+	public void setCityBonusNumberMax(int cityBonusNumberMax) {
+		this.cityBonusNumberMax = cityBonusNumberMax;
+	}
+
+	public int getNumberCities() {
+		return citiesNumber;
+	}
+
+	public void setCitiesNumber(int numberCities) {
+		this.citiesNumber = numberCities;
+	}
+	
+	public int getPermitCardBonusNumberMin() {
+		return permitCardBonusNumberMin;
+	}
+
+	public void setPermitCardBonusNumberMin(int permitCardBonusNumberMin) {
+		this.permitCardBonusNumberMin = permitCardBonusNumberMin;
+	}
+
+	public int getPermitCardBonusNumberMax() {
+		return permitCardBonusNumberMax;
+	}
+
+	public void setPermitCardBonusNumberMax(int permitCardBonusNumberMax) {
+		this.permitCardBonusNumberMax = permitCardBonusNumberMax;
+	}
+	
+	public boolean isNobilityBonusRandom() {
+		return nobilityBonusRandom;
+	}
+
+	public int getNobilityBonusNumber() {
+		return nobilityBonusNumber;
+	}
+
+	public boolean isCityLinksPreconfigured() {
+		return cityLinksPreconfigured;
+	}
+
+	public void setCityLinksPreconfigured(boolean cityLinksPreconfigured) {
+		this.cityLinksPreconfigured = cityLinksPreconfigured;
+	}
+
+	public int[][] getCityLinksMatrix() {
+		return cityLinksMatrix;
+	}
+
+	public void setCityLinksMatrix(int[][] cityLinksMatrix) {
+		this.cityLinksMatrix = cityLinksMatrix;
+	}
+
+	public boolean isCityBonusRandom() {
+		return cityBonusRandom;
+	}
+
+	public void setCityBonusRandom(boolean cityBonusRandom) {
+		this.cityBonusRandom = cityBonusRandom;
+	}
+
+	public HashMap<String, Integer>[] getCityBonusArrayMaps() {
+		return cityBonusArrayMaps;
+	}
+
+	public void setCityBonusArrayMaps(HashMap<String, Integer>[] cityBonusArrayMaps) {
+		this.cityBonusArrayMaps = cityBonusArrayMaps;
+	}
+
+	public int getCitiesNumber() {
+		return citiesNumber;
+	}
+
+	public void setNobilityBonusRandom(boolean nobilityBonusRandom) {
+		this.nobilityBonusRandom = nobilityBonusRandom;
+	}
+
+	public void setNobilityBonusNumber(int nobilityBonusNumber) {
+		this.nobilityBonusNumber = nobilityBonusNumber;
+	}
+	
+	
+}
