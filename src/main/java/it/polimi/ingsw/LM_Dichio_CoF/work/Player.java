7458ad9a367418f2ada9602b1049fd32ba011133
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-31 00:06:33 +0200

    Creato alcune variabili di gioco (richness, assistant, card) ed alcuni metodi relativi

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Player.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Player.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work;
 
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Player {
 
-	private String id;
 	private String nickname;
 	
 	private char typeOfConnection;
@@ -12,9 +12,14 @@ public class Player {
 	
 	private char CLIorGUI;
 
+	
+	
 	// variables of the game
 	private int richness;
 	private int assistant;
+	private ArrayList <PoliticCard> arrayListPoliticCard= new ArrayList<>();
+	
+	
 	
 	/* The constructor assigns to the player the type of connection */
 	public Player(String typeOfConnection){ this.typeOfConnection=typeOfConnection.charAt(0); }
@@ -30,5 +35,21 @@ public class Player {
 
 	public char getTypeOfConnection() {return typeOfConnection;}
 	public void setTypeOfConnection(char typeOfConnection) {this.typeOfConnection = typeOfConnection;}
+
+	
+	
+	public int getRichness() {return richness;}
+	public void setRichness(int richness) {this.richness = richness;}
+
+	public int getAssistant() {	return assistant;}
+	public void setAssistant(int assistant) {this.assistant = assistant;}
+	public void addAssistant(int increment)	{this.assistant +=increment;}
+
+	public ArrayList<PoliticCard> getArrayListPoliticCard() {return arrayListPoliticCard;}
+	public void addPoliticCard(PoliticCard politicCard) {this.arrayListPoliticCard.add(politicCard);}
+	
+	
+	
+	
 	
 }
