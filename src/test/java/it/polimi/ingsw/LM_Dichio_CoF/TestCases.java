5a1e9aec836d2c2c385916302a094b04e69679aa
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-30 23:01:30 +0200

    Ho cambiato i test per supportare il file di configurazione, ora per avere combinazioni di test differenti basta cambiare i dati in "createConfigurations()"

--- src/test/java/it/polimi/ingsw/LM_Dichio_CoF/TestCases.java
+++ src/test/java/it/polimi/ingsw/LM_Dichio_CoF/TestCases.java
@@ -1,8 +1,15 @@
 package it.polimi.ingsw.LM_Dichio_CoF;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.Test;
 
 import it.polimi.ingsw.LM_Dichio_CoF.work.Configurations;
 import it.polimi.ingsw.LM_Dichio_CoF.work.Player;
@@ -10,20 +17,28 @@ import it.polimi.ingsw.LM_Dichio_CoF.work.field.*;
 
 public class TestCases {
 	
+	Configurations config;
 	
 	public Configurations configurations(){
 		
-		// Parameters constructor: numberCities, minNumberBonusPerCity,
-		// maxNumberBonusPerCity
-		Configurations config = new Configurations(15,"n".charAt(0), 0,3,1,2);
+		/*
+		 * These 3 methods permit to create the configurations, write them in a file 
+		 * and read them. They substitute the action of the player (player side),
+		 * that creates the file, and the
+		 * class "Match" that reads the file
+		 * If someone wants to change the configurations to see the different test cases
+		 * he can do it in the method "createConfigurations"
+		 */
+		createConfigurations();
+		createFileConfigurations();
+		readFileConfigurations();
+
 		return config;
-		
 	}
-	
-	
+
 	public City[] arrayCity(){
 		
-		Configurations config = configurations();
+		config = configurations();
 		Map <String, Integer> bonusMap =new HashMap <> ();
 		
 		
@@ -36,16 +51,12 @@ public class TestCases {
 		City[] arrayCity = new City[n];
 		for(int i=0; i<n; i++){
 			
-			//arrayCity[i] = new City(config, CityName.getCityName(i), RegionName.Sea, CityColor.Blue);
+			//arrayCity[i] = new City(config, CityName.getCityNameFromIndex(i), RegionName.Sea, CityColor.Blue);
 			arrayCity[i] = new City(bonusMap, CityName.getCityNameFromIndex(i), RegionName.Sea, CityColor.Blue);
 		}
-		
+		System.out.println();
 		return arrayCity;
 	}
-	
-	
-	
-	
 	
 	public ArrayList <Player> arrayListPlayer() {
 	
@@ -62,6 +73,116 @@ public class TestCases {
 		arrayListPlayer.add(player3);
 		
 		return arrayListPlayer;
+	}
+	
+	private void createConfigurations(){
+		
+		config = new Configurations();
+		
+		/*
+		 * Do not change this parameter and the difficulty one until we haven't create 
+		 * new maps for those combination missing
+		 */
+		config.setCitiesNumber(15);
+		
+		config.setPermitCardBonusNumberMin(2);
+		config.setPermitCardBonusNumberMax(3);
+		
+		config.setNobilityBonusRandom(false);
+		if(config.isNobilityBonusRandom()==false){
+			config.setNobilityBonusNumber(7);
+		}
+		
+		config.setCityLinksPreconfigured(false);
+		if(config.isCityLinksPreconfigured()==false){
+			int[][]cityLinksMatrix =  new int[][]{
+				{0,	1,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	1,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	1,	0,	1,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	1,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0}
+			};
+			/*
+    		 * This fir cycle is for making the matrix specular,
+    		 * because in the txt file it is only upper triangular set
+    		 */
+			for(int i=0; i<config.getCitiesNumber(); i++){
+				for(int j=i; j<config.getCitiesNumber();j++){
+					cityLinksMatrix[j][i]=cityLinksMatrix[i][j];
+				}
+			}
+			config.setCityLinksMatrix(cityLinksMatrix);
+			
+		}else{
+			config.setDifficulty("n".charAt(0));
+		}
+		
+		config.setCityBonusRandom(true);
+		if(config.isCityBonusRandom()==false){
+			
+			//da implementare, ma essendo un esempio non ha senso farlo ora (l'array di mappe)
+		}else{
+			config.setCityBonusNumberMin(2);
+			config.setCityBonusNumberMax(3);
+		}
+		
+	}
+	
+	private void createFileConfigurations(){
+		
+		FileOutputStream fileOutputStream;
+		
+		try {
+			
+			//il salvataggio per ora è in locale, dovrà essere inviato al server quando ci sarà la connessione
+			fileOutputStream = new FileOutputStream("./src/configurations/config");
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+			// write something in the file
+			objectOutputStream.writeObject(config);
+
+			// close the stream
+			fileOutputStream.close();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void readFileConfigurations(){
+		
+		FileInputStream fileInputStream;
+		
+		try {
+			
+			//il salvataggio per ora è in locale, dovrà essere inviato al server quando ci sarà la connessione
+			fileInputStream = new FileInputStream("./src/configurations/config");
+			
+			// create an ObjectInputStream for the file we created before
+	         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+	         this.config = (Configurations) objectInputStream.readObject();
+	         
+	         fileInputStream.close();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
