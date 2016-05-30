3d4e628d0d723ef3fac44a5ece2ebb04a5f591e8
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-30 23:00:45 +0200

    C'è un esempio di creazione (definizione e scrittura) del file configurazione

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/PlayerSide.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/PlayerSide.java
@@ -1,18 +1,27 @@
 package it.polimi.ingsw.LM_Dichio_CoF_PlayerSide;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class PlayerSide {
 
 	private  Socket mySocket;
 	private final static String address="localhost";
+	private Configurations config;
 	
 	public PlayerSide() {
 		
 		System.out.println("I am alive");	
 		
-		connectToServer();
+		
+		// questi due metodi andranno dopo la connessione
+		createConfigurations();
+		createFileConfigurations();
+		
+		//connectToServer();
 	
 	}
 
@@ -23,6 +32,7 @@ public class PlayerSide {
 			mySocket = new Socket(address,3000);
 			
 			System.out.println("I am connected to the Server");
+			
 			
 			
 			
@@ -37,6 +47,86 @@ public class PlayerSide {
 		
 		new PlayerSide();
 		
+	}
+	
+	private void createConfigurations(){
+		
+		config = new Configurations();
+		
+		config.setCitiesNumber(15);
+		
+		config.setPermitCardBonusNumberMin(2);
+		config.setPermitCardBonusNumberMax(3);
+		
+		config.setNobilityBonusRandom(false);
+		if(config.isNobilityBonusRandom()==false){
+			config.setNobilityBonusNumber(7);
+		}
+		
+		config.setCityLinksPreconfigured(false);
+		if(config.isCityLinksPreconfigured()==false){
+			int[][]cityLinksMatrix =  new int[][]{
+				{0,	1,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	1,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	1,	0,	1,	0,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	1,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+				{0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0}
+			};
+			/*
+    		 * This fir cycle is for making the matrix specular,
+    		 * because in the txt file it is only upper triangular set
+    		 */
+			for(int i=0; i<config.getCitiesNumber(); i++){
+				for(int j=i; j<config.getCitiesNumber();j++){
+					cityLinksMatrix[j][i]=cityLinksMatrix[i][j];
+				}
+			}
+			config.setCityLinksMatrix(cityLinksMatrix);
+			
+		}else{
+			config.setDifficulty("n".charAt(0));
+		}
+		
+		config.setCityBonusRandom(true);
+		if(config.isCityBonusRandom()==false){
+			//da implementare, ma essendo un esempio non ha senso farlo ora (l'array di mappe)
+		}else{
+			config.setCityBonusNumberMin(2);
+			config.setCityBonusNumberMax(3);
+		}
+		
+	}
+	
+	private void createFileConfigurations(){
+		
+		FileOutputStream fileOutputStream;
+		
+		try {
+			
+			//il salvataggio per ora è in locale, dovrà essere inviato al server quando ci sarà la connessione
+			fileOutputStream = new FileOutputStream("./src/configurations/config");
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+			// write something in the file
+			objectOutputStream.writeObject(config);
+
+			// close the stream
+			fileOutputStream.close();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
