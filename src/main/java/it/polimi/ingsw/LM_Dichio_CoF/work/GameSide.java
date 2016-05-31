6edec78ea21096a07cf480e4a21e0e158ddec224
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-05-31 23:51:21 +0200

    Aggiunta la lettura del file di configurazione inviato da socket e la scrittura nel file di destinazione

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/GameSide.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/GameSide.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -18,8 +20,8 @@ public class GameSide {
 		try {
 			serverSocket = new ServerSocket(Constant.SOCKET_PORT);
 			while(true){
-				Socket clientSocket = serverSocket.accept();
-				new SocketConnectionWithClient(clientSocket, this);		
+				Socket socket = serverSocket.accept();
+				new SocketConnectionWithClient(socket, this);		
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -34,12 +36,33 @@ public class GameSide {
 	
 		System.out.println("I am managing a player through a thread");
 		
-		player.send("ciao");
-		
 		if(arrayListAvailablePlayer.size()==1){
-			//method that asks for configurations
+			askForConfigurations(player);
+		}else{
+			player.sendString("Sei arrivato tardi");
 		}
 			
+	}
+	
+	private synchronized/*??*/ void askForConfigurations(Player player){
+		
+		player.sendString("configRequest");
+		
+		Object object = player.receiveObject();
+		FileOutputStream fileOutputStream = null;
+		try {
+			fileOutputStream = new FileOutputStream("./src/configurations/config");
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+			objectOutputStream.writeObject(object);
+		} catch (IOException e) {
+			e.printStackTrace();	
+		}finally{
+			try {
+				fileOutputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	
