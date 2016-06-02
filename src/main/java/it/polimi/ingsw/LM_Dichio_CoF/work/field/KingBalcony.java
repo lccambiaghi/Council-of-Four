c5cedf6677445f50c0443ee3d229e658606e02c7
Luca Cambiaghi <luca.cambiaghi@mail.polimi.it>
2016-06-02 17:20:44 +0200

    Refactor availableCouncillor -> availableCouncillors

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/KingBalcony.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/KingBalcony.java
@@ -2,11 +2,11 @@ package it.polimi.ingsw.LM_Dichio_CoF.work.field;
 
 public class KingBalcony extends Balcony {
 
-	AvailableCouncillor availableCouncillor;
+	AvailableCouncillors availableCouncillors;
 
 	/* The constructor creates the balcony like the superclass */
-	public KingBalcony(AvailableCouncillor availableCouncillor, String nameBalcony) {
-		super(availableCouncillor, nameBalcony);
+	public KingBalcony(AvailableCouncillors availableCouncillors, String nameBalcony) {
+		super(availableCouncillors, nameBalcony);
 	}
 	
 	
