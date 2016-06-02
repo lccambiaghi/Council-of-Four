9f6d6be2c3f0cc58106076faa4791e69964543e8
Luca Cambiaghi <luca.cambiaghi@mail.polimi.it>
2016-06-02 20:09:47 +0200

    RemoveAvailableCouncillor, AddAvailableCouncillor, CheckIfColorAvailable

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/AvailableCouncillors.java
@@ -0,0 +1,89 @@
+package it.polimi.ingsw.LM_Dichio_CoF.work.field;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import it.polimi.ingsw.LM_Dichio_CoF.work.Color;
+import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
+
+public class AvailableCouncillors {
+
+	/* Singleton DESIGN PATTERN
+	   The constructor is private, so that it is inaccessible from outside,
+	   Once the class is created for the first time, the method "createList()" is invoked */
+	
+	private ArrayList <Councillor> arrayListCouncillor;
+
+	// NON POSSO METTERLO SINGLETON :( o si? :D
+	/*
+	private static AvailableCouncillors instance = null;
+	private AvailableCouncillors() { }
+	public static AvailableCouncillors getInstance() {
+		if (instance == null) {
+			instance = new AvailableCouncillors();
+			createList();
+		}
+		return instance;
+	}
+	*/
+
+	/* The constructor creates councillors in order in the arrayList and then
+	  shuffles it */
+	public AvailableCouncillors(){
+		arrayListCouncillor = new ArrayList<Councillor>();
+		int councilorsPerColor = Constant.COUNCILLORS_NUMBER_TOTAL /Constant.COLORS_NUMBER;
+
+		for(int i = 0; i< Constant.COUNCILLORS_NUMBER_TOTAL; i++){
+			int indexOfColor =(i/councilorsPerColor);
+			Councillor councillor = new Councillor(Color.getColorFromIndex(indexOfColor));
+			arrayListCouncillor.add(councillor);
+		}
+
+		Collections.shuffle(arrayListCouncillor);
+	}
+
+	public void addAvailableCouncillor(Councillor councillor){
+		arrayListCouncillor.add(councillor);
+	}
+
+	/* This method returns the first Councillor of the ArrayList, removing it */
+	public Councillor removeAvailableCouncillor(){ return arrayListCouncillor.remove(0);}
+
+	//Overloading
+	/* This method returns the Councillor of the specified color if present, else null */
+	public Councillor removeAvailableCouncillor(Color color) {
+
+		Councillor councillor;
+		// If checkIfColorAvailable(Color color)
+		for (int i = 0; i < arrayListCouncillor.size(); i++) {
+			councillor = arrayListCouncillor.get(i);
+			if (councillor.getColor() == color)
+				return arrayListCouncillor.remove(i);
+		}
+
+		return null;
+
+	}
+
+	public boolean checkIfColorAvailable(Color color){
+
+		Councillor councillor;
+
+		for (Councillor anArrayListCouncillor : arrayListCouncillor) {
+			if (anArrayListCouncillor.getColor() == color)
+				return true;
+		}
+
+		return false;
+
+	}
+
+	public ArrayList<Councillor> getArrayListCouncillor() {
+		return arrayListCouncillor;
+	}
+
+
+
+
+
+}
