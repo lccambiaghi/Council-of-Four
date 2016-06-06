e195ad18c72d19c2f83f4dd412849bf83a1c3173
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-06-06 19:03:16 +0200

    Inizio di creazione rmi

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/RMIConnectionWithPlayer.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.LM_Dichio_CoF.connection;
+
+import it.polimi.ingsw.LM_Dichio_CoF.work.GameSide;
+import it.polimi.ingsw.LM_Dichio_CoF.work.Player;
+
+public class RMIConnectionWithPlayer extends Thread{
+
+	GameSide gameSide;
+	Player player;
+	
+	public RMIConnectionWithPlayer(GameSide gameSide){
+		this.gameSide=gameSide;
+		
+		while(true){
+			
+		}
+		
+	}
+	
+	
+	
+}
