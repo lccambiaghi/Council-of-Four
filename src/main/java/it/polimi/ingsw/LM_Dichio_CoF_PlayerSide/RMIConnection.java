1021b43d7973d7d37df5104360054a2fb4960de4
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-06-06 19:07:31 +0200

    Da implementare (RMI)

---
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/RMIConnection.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.LM_Dichio_CoF_PlayerSide;
+
+public class RMIConnection {
+
+	PlayerSide playerSide;
+	
+	public RMIConnection(PlayerSide playerSide){
+		this.playerSide=playerSide;
+	}
+	
+}
