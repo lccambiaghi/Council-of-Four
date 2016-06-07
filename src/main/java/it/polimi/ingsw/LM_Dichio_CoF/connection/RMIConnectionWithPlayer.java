60147755fce87db7b23543ff3ea729662ba0b912
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-06-08 01:14:39 +0200

    Ora gestisce  la connessione RMI lanciando il thread di sempre

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/RMIConnectionWithPlayer.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/RMIConnectionWithPlayer.java
@@ -2,23 +2,29 @@ package it.polimi.ingsw.LM_Dichio_CoF.connection;
 
 import it.polimi.ingsw.LM_Dichio_CoF.work.GameSide;
 import it.polimi.ingsw.LM_Dichio_CoF.work.Player;
+import it.polimi.ingsw.LM_Dichio_CoF_PlayerSide.RMIPlayerSideInterface;
 
 public class RMIConnectionWithPlayer extends Thread{
 
 	GameSide gameSide;
 	Player player;
 	
-	public RMIConnectionWithPlayer(GameSide gameSide){
-		
+	public RMIConnectionWithPlayer(RMIPlayerSideInterface playerSide, GameSide gameSide){
+	
 		this.gameSide=gameSide;
 		
+		player = new Player('r');
+		player.setPlayerSide(playerSide);
 		
-		while(true){
-			
-		}
+		start();
 		
 	}
 	
+	public void run(){
+		
+		gameSide.handlePlayer(player);
+		
+	}
 	
 	
 }
