57940d1acc5181598106666da27b183278823a11
ale_dichio <alessio.dichio@mail.polimi.it>
2016-06-07 23:33:37 +0200

    implementato il costruttore. Aggiunto il metodo start market che chiede ai singoli giocatori cosa vogliono vendere. Da completare

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Market.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/work/Market.java
@@ -1,10 +1,24 @@
 package it.polimi.ingsw.LM_Dichio_CoF.work;
 
 import it.polimi.ingsw.LM_Dichio_CoF.work.field.SellingObject;
+import java.util.ArrayList;
 
 public class Market {
 
 	SellingObject sellingObject[];
+	ArrayList <Player> arrayListPlayer= new ArrayList<>();
+		
+	Market (ArrayList <Player> arrayListPlayer){
+		this.arrayListPlayer=arrayListPlayer;
+	}
 	
+	private void startMarket (){
+		int turn = 0;
+		
+		Player playerTurn=arrayListPlayer.get(turn);
+		
+		SellingObject object = new SellingObject(playerTurn);
+		
+	}
 	
 }
