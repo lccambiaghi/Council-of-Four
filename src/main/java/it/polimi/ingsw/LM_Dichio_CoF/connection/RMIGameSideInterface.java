cfc6966fb5c4e99da0956ac7af8ff582335abc9e
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-06-08 18:49:08 +0200

    Tolto i metodi non utilizzati

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/RMIGameSideInterface.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/connection/RMIGameSideInterface.java
@@ -8,10 +8,4 @@ public interface RMIGameSideInterface extends Remote{
 
 	public void connectToServer(RMIPlayerSideInterface playerSide) throws RemoteException;
 	
-	//public void sendStringTS(String string) throws RemoteException;
-	
-	//public String receiveStringFS() throws RemoteException;
-	
-	//public void sendObjectTS(Object object) throws RemoteException;
-	
 }
