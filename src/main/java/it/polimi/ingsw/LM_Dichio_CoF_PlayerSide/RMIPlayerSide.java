175942766459091b2f3f9134546efdf9194b150d
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-06-08 18:52:39 +0200

    Nuovi metodi remoti che può chiamare il server

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/RMIPlayerSide.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/RMIPlayerSide.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
 
 public class RMIPlayerSide extends UnicastRemoteObject implements RMIPlayerSideInterface {
 
-	public RMIPlayerSide() throws RemoteException {}
+	PlayerSide playerSide;
+	
+	public RMIPlayerSide(PlayerSide playerSide) throws RemoteException {
+		this.playerSide=playerSide;
+	}
 
 	public void sendString(String string){
 		System.out.println(string);
@@ -15,9 +19,29 @@ public class RMIPlayerSide extends UnicastRemoteObject implements RMIPlayerSideI
 	public String receiveString (){
 		
 		Scanner inCLI = new Scanner(System.in);
-		//Mi sa che qui ci vuole un controllo......
+		//Mi sa che qui ci vuole un controllo sull'input
 		String string = inCLI.nextLine();
 		return string;
+	}
+	
+	public void waitForServer(){
+		playerSide.waitForServer();
+	}
+	
+	public void configure(){	
+		this.playerSide.configure();	
+	}
+	
+	public int getConfigurationsPlayersNumber(){
+		return playerSide.getConfigurationsPlayersMaxNumber();
+	}
+	
+	public Object getConfigurationsAsObject(){
+		return playerSide.getConfigurationsAsObject();
+	}
+	
+	public void startingMatch(){
+		playerSide.startingMatch();
 	}
 	
 }
