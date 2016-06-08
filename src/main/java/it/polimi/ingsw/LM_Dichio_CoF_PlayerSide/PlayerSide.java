9b1426101a7bc7ffd4d3ccaa38943969348f82cb
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-06-08 18:51:40 +0200

    Le configurazioni sono solo standard. Aggiunto implementazione nello switch per i messaggi socket.

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/PlayerSide.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/PlayerSide.java
@@ -19,11 +19,14 @@ import it.polimi.ingsw.LM_Dichio_CoF.work.GameSide;
 
 public class PlayerSide {
 
-	RMIGameSideInterface gameSide;
 	
 	public static void main (String[] args){
 		new PlayerSide();
 	}
+	
+	public void printString(String string) { System.out.println(string); }
+	
+	RMIGameSideInterface rmiGameSide;
 	
 	char typeOfConnection;
 	
@@ -41,7 +44,7 @@ public class PlayerSide {
 	
 	public PlayerSide() {
 		
-		System.out.println("I am alive");	
+		printString("I am alive");	
 		
 		inCLI = new Scanner(System.in);
 		
@@ -52,17 +55,9 @@ public class PlayerSide {
 		chooseConnection();
 		
 		if(typeOfConnection=='s'){
-			//socketConnection = new SocketConnection(this);
+			socketConnection = new SocketConnection(this);
 		}else{
 			rmiConnection = new RMIConnection(this);
-			gameSide=rmiConnection.getGameSide();
-			System.out.println("I got the RMI");
-			
-			try {
-				rmiPlayerSide = new RMIPlayerSide();
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
 		}
 		
 		communicateWithServer();
@@ -73,66 +68,59 @@ public class PlayerSide {
 	
 	private void communicateWithServer(){
 		
-		try {
-			gameSide.connectToServer(rmiPlayerSide);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-		
-		login();
-		
-		while(true){
-			message = receiveStringFS();
-			switch(message){
-				case 	"config":
-					
-					boolean standardConfig = false;
-					/* 
-					 * It starts a thread to manage the creation of configurations
-					 * This way server says that the time is over the thread will stop
-					 * The timer is not fixed to 20 seconds, this is a server's problem
-					 * I have to implement another timer:
-					 * when the second player arrives a new timer starts (for example 1 minute)
-					 * so that the other players don't have to wait to much for a new match, whose config
-					 * are created by the first player.
-					 * If the player can't set them on time then he will send a standard configuration
-					 */
-					//CreateConfigurations createConfigurations = new CreateConfigurations(this);
-					//Thread threadCreateConfigurations = new Thread(createConfigurations);
-					//threadCreateConfigurations.start();
-					
-					//Case: client can't make the config on time
-					///// Here I need a method to stop the thread
-					System.out.println(/*"Too late,"*/" standard configurations will be used");
-					standardConfig = true;
-					if(standardConfig){
-						setStandardPlayersNumber();
-						setStandardConfigurations();
-					}
-					sendStringTS(""+playersMaxNumber);
-					sendObjectTS(config);
+		if(typeOfConnection=='s'){
+			/*
+			 * If SOCKET is chosen
+			 */
+			while(true){
+				message = receiveStringFS();
+				switch(message){
+				
+				case	"SOCKETlogin":
+					SOCKETlogin();
 					break;
 				
-				case 	"wait":
-					System.out.println("Just wait a moment");
+				case	"SOCKETwaitForServer":
+					waitForServer();
+					break;
+					
+				case 	"SOCKETconfigure":
+					configure();
+					break;
+					
+				case	"SOCKETgetConfigurationsPlayersMaxNumber":
+					sendStringTS(String.valueOf(getConfigurationsPlayersMaxNumber()));
+					break;
+					
+				case	"SOCKETgetConfigurationsAsObject":
+					sendObjectTS(getConfigurationsAsObject());
 					break;
 				
-				case 	"startingMatch":
-					System.out.println("You are waiting for a match to start");
+				case 	"SOCKETstartingMatch":
+					startingMatch();
 					break;
 					
 				default	:
 					System.out.println("error");
 					break;
-					
+				}
 			}
+			
+		}else{
+			/*
+			 * If RMI is chosen
+			 */
+			rmiGameSide=rmiConnection.getRmiGameSide();
+		
 		}
-	}
 	
-	public void chooseConnection(){
+	}
+		
+	
+	private void chooseConnection(){
 		boolean chosen = false;
 		while(!chosen){
-			System.out.println("Choose connection: 's' (Socket) or 'r' (RMI)");
+			printString("Choose connection: 's' (Socket) or 'r' (RMI)");
 			String in = inCLI.nextLine();
 			if(in.equals("s")||in.equals("r")){
 				this.typeOfConnection=in.charAt(0);
@@ -141,70 +129,130 @@ public class PlayerSide {
 		}
 	}
 	
-	
+	/*
+	 * Methods only used by the connection SOCKET
+	 */
 	// TS= To Server, FS= From Server
 	private void sendStringTS(String string){
-		if(typeOfConnection=='s'){
-			socketConnection.sendStringTS(string);
-		}
+		socketConnection.sendStringTS(string);
 	}
+		
 	private String receiveStringFS(){
-		if(typeOfConnection=='s'){
-			return socketConnection.receiveStringFS();
-		}
-		else
-			return "error";
+		return socketConnection.receiveStringFS();
 	}
 	
 	private void sendObjectTS(Object object){
-		if(typeOfConnection=='s'){
-			socketConnection.sendObjectTS(object);;
-		}
+		socketConnection.sendObjectTS(object);
 	}
 	
-	private void login(){
+	private void SOCKETlogin(){
 		boolean logged = false;
 		while(!logged){
-			System.out.println("Enter a valid nickname");
+			printString("Enter your nickname");
 			String nickname = inCLI.nextLine();
-			sendStringTS("login");
 			sendStringTS(nickname);
-			logged = Boolean.valueOf(receiveStringFS());
+			String received = receiveStringFS();
+			logged = Boolean.valueOf(received);
 			if(!logged){
-				System.out.println("Nickname already in use");
+				printString("Nickname already in use, enter another one");
 			}
-		}	
+		}
+		printString("Logged!");
 	}
 	
-	public void setPlayersNumber(int playersMaxNumber){ this.playersMaxNumber= playersMaxNumber; }
+	
+	public void waitForServer(){
+		printString("Just wait a moment...");
+	}
+	
+	public void configure(){
+		printString("You are the first player, standard configurations will be used (FOR THE MOMENT)");
+	}	
+	
+	public int getConfigurationsPlayersMaxNumber(){
+		printString("(FOR THE MOMENT) the standard players number will be used");
+		setStandardPlayersMaxNumber();
+		return playersMaxNumber;
+	}
+	
+	public Object getConfigurationsAsObject(){
+		printString("(FOR THE MOMENT) the standard configurations will be used");
+		setStandardConfigurations();
+		return config;
+	}
+	
+	public void startingMatch(){
+		printString("You have been selected for a match, wait a moment...");
+	}
+	
+	
+	
+	//Those two methods are not used for the moment
+	public void setPlayersMaxNumber(int playersMaxNumber){ this.playersMaxNumber= playersMaxNumber; }
 	public void setConfigurations(Configurations config){ this.config = config; }
 		
-	public void setStandardPlayersNumber(){ this.playersMaxNumber= 4;}
+	public void setStandardPlayersMaxNumber(){ this.playersMaxNumber= 4;}
 		
 	private void setStandardConfigurations(){
 		
 		FileInputStream fileInputStream = null;
+		ObjectInputStream objectInputStream = null;
 		try {
 
-			fileInputStream = new FileInputStream("./src/playerConfigurations/config");
-	        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+			//This part permit to read the file of the standard configurations
+			fileInputStream = new FileInputStream("./src/playerConfigurations/standardConfig");
+	        objectInputStream = new ObjectInputStream(fileInputStream);
 	        this.config = (Configurations) objectInputStream.readObject();
 		
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally{
-			// close the stream
 			try {
 				fileInputStream.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
+	
+	
+	
+	
+	/*	
+	standardConfig = true;
+	if(standardConfig){
+		setStandardPlayersNumber();
+		setStandardConfigurations();
+	}
+	sendStringTS(""+playersMaxNumber);
+	sendObjectTS(config);
+	}*/
+	/* 
+	 * It starts a thread to manage the creation of configurations
+	 * This way server says that the time is over the thread will stop
+	 * The timer is not fixed to 20 seconds, this is a server's problem
+	 * I have to implement another timer:
+	 * when the second player arrives a new timer starts (for example 1 minute)
+	 * so that the other players don't have to wait to much for a new match, whose config
+	 * are created by the first player.
+	 * If the player can't set them on time then he will send a standard configuration
+	 */
+	//CreateConfigurations createConfigurations = new CreateConfigurations(this);
+	//Thread threadCreateConfigurations = new Thread(createConfigurations);
+	//threadCreateConfigurations.start();
+	
+	//Case: client can't make the config on time
+	///// Here I need a method to stop the thread
+	//printString(/*"Too late,"*/"standard configurations will be used");
+	/*standardConfig = true;
+	if(standardConfig){
+		setStandardPlayersNumber();
+		setStandardConfigurations();
+	}
+	sendStringTS(""+playersMaxNumber);
+	sendObjectTS(config);
+	*/
 	
 }
