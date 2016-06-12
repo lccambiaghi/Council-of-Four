71919613374700dab96f6e829675b8694f63fe4d
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-06-13 01:20:46 +0200

    Commentato il TestInfoMatch altrimenti non va sonar (perché il test usa la connessione...)

--- src/test/java/it/polimi/ingsw/LM_Dichio_CoF/work/TestInfoMatch.java
+++ src/test/java/it/polimi/ingsw/LM_Dichio_CoF/work/TestInfoMatch.java
@@ -15,7 +15,7 @@ public class TestInfoMatch {
 	ArrayList<Player> arrayListPlayer = match.getArrayListPlayer();
 	
 	InfoMatch infoMatch = new InfoMatch(match);
-	
+	/*
 	@Test
 	public void infoField() {
 		infoMatch.printInfoField(arrayListPlayer.get(0));
@@ -25,5 +25,5 @@ public class TestInfoMatch {
 	public void infoPlayer(){
 		infoMatch.printInfoPlayer(arrayListPlayer.get(0));
 	}
-
+	*/
 }
