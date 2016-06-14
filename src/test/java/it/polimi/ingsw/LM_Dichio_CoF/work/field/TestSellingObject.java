68068b90a498ba42123a8f51619c656e408545fe
ale_dichio <alessio.dichio@mail.polimi.it>
2016-06-14 16:05:23 +0200

    implementati i test per i vari object

--- src/test/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/TestSellingObject.java
+++ src/test/java/it/polimi/ingsw/LM_Dichio_CoF/work/field/TestSellingObject.java
@@ -6,6 +6,8 @@ import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 
 import it.polimi.ingsw.LM_Dichio_CoF.work.Constant;
+import it.polimi.ingsw.LM_Dichio_CoF.work.InfoMatch;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -18,9 +20,9 @@ public class TestSellingObject {
 	private TestCases testCases = new TestCases();
     private ArrayList <Player> arrayListPlayer= testCases.arrayListPlayer();
     private MatchMock match = testCases.matchMock();
-
-    @Ignore
-	public void constructor() {
+    
+    @Test
+	public void constructorPolitic() {
 
 		//ByteArrayInputStream in=new ByteArrayInputStream("1\n5\n".getBytes());
 		//System.setIn(in);
@@ -33,8 +35,44 @@ public class TestSellingObject {
 		assertEquals(5,sellingObject.getPrice());
 
 		assertEquals(Constant.POLITIC_CARDS_INITIAL_NUMBER-1, arrayListPlayer.get(0).getArrayListPoliticCard().size());
+	    
+		InfoMatch info = new InfoMatch(match);
 
-		System.setIn(System.in);
+		//System.setIn(System.in);
+	}
+    @Ignore
+    public void constructorAssistant() {
+
+		//ByteArrayInputStream in=new ByteArrayInputStream("1\n5\n".getBytes());
+		//System.setIn(in);
+
+    	assertEquals(Constant.ASSISTANT_INITIAL_FIRST_PLAYER, arrayListPlayer.get(0).getAssistant());
+    	
+		SellingObject sellingObject=new SellingObject (arrayListPlayer.get(0), "Assistants");
+
+		assertEquals("Integer",sellingObject.getObject().getClass().getSimpleName());
+		assertEquals(5,sellingObject.getPrice());
+
+		assertEquals(Constant.ASSISTANT_INITIAL_FIRST_PLAYER-1, arrayListPlayer.get(0).getAssistant());
+
+		//System.setIn(System.in);
+	}
+    @Ignore
+    public void constructorPermitCard() {
+
+		//ByteArrayInputStream in=new ByteArrayInputStream("1\n5\n".getBytes());
+		//System.setIn(in);
+
+    	assertEquals(1, arrayListPlayer.get(0).getArrayListPermitCard().size());
+    	
+		SellingObject sellingObject=new SellingObject (arrayListPlayer.get(0), "PermitCard");
+
+		assertEquals("PermitCard",sellingObject.getObject().getClass().getSimpleName());
+		assertEquals(5,sellingObject.getPrice());
+
+		assertEquals(0, arrayListPlayer.get(0).getArrayListPermitCard().size());
+
+		//System.setIn(System.in);
 	}
 
 }
