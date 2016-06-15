525ea3edf7f92950e2664ca565fc8baf82fe001b
ale_dichio <alessio.dichio@mail.polimi.it>
2016-06-15 19:15:07 +0200

    Implementazione Elect Councillor with Asssistant Quick Action

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/actions/ElectCouncillorWithAssistantQuickAction.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/actions/ElectCouncillorWithAssistantQuickAction.java
@@ -1,10 +1,25 @@
 package it.polimi.ingsw.LM_Dichio_CoF.control.actions;
 
+import static it.polimi.ingsw.LM_Dichio_CoF_PlayerSide.InputHandler.inputNumber;
+
+import java.util.ArrayList;
+
+import it.polimi.ingsw.LM_Dichio_CoF.connection.Broker;
+import it.polimi.ingsw.LM_Dichio_CoF.control.Constant;
+import it.polimi.ingsw.LM_Dichio_CoF.control.Message;
 import it.polimi.ingsw.LM_Dichio_CoF.control.Player;
+import it.polimi.ingsw.LM_Dichio_CoF.model.Color;
 import it.polimi.ingsw.LM_Dichio_CoF.model.Match;
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.AvailableCouncillors;
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.Balcony;
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.Councillor;
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.Field;
 
 public class ElectCouncillorWithAssistantQuickAction extends Action {
 
+	private Balcony chosenBalcony;
+	private Color chosenCouncillorColor;
+	
     public ElectCouncillorWithAssistantQuickAction(Match match, Player player){
         this.match=match;
         this.player=player;
@@ -12,13 +27,74 @@ public class ElectCouncillorWithAssistantQuickAction extends Action {
 
     @Override
     public boolean preliminarySteps(){
+    	
+    	if (player.getAssistant()<Constant.ELECTION_ASSISTANT_COST){
+			Message.notEnoughAssistant(player);
+			return false;
+		}
+		chosenBalcony = chooseBalcony();
+		ArrayList<Color> choosableColors = getChoosableColors();
+		
+		if (choosableColors.size()<1)
+	        return false;
+		
+		chosenCouncillorColor=chooseCouncillorColor(choosableColors);
 
-        return false;
+        return true;
+    }
+    
+    @Override
+    public void execute(){
+    	Field field = match.getField();
+    	AvailableCouncillors availableCouncillors = field.getAvailableCouncillors();
+		Councillor chosenCouncillor = availableCouncillors.removeAvailableCouncillor(chosenCouncillorColor); //NullPointerException?
+		
+		chosenBalcony.electCouncillor(chosenCouncillor,availableCouncillors);
+    	player.decrementAssistant(Constant.ELECTION_ASSISTANT_COST);
+    	
+    	resultMsg="Player "+player.getNickname() +"has elected a councillor paying"
+    			+"an Assistant";
     }
 
     @Override
-    public void execute(){
+    public String getResultMsg(){return resultMsg;}
+       
+    private Balcony chooseBalcony(){
+
+        Field field=match.getField();
+
+        Message.chooseBalcony_1_4(player);
+
+        return field.getBalconyFromIndex(inputNumber(1, 4)-1); //-1 for array positioning
 
     }
+
+    private ArrayList<Color> getChoosableColors() {
+
+        Field field=match.getField();
+        ArrayList<Councillor> availableCouncillors = field.getAvailableCouncillors().getArrayListCouncillor();
+        boolean[] seen = new boolean[Constant.COLORS_NUMBER];
+
+        ArrayList<Color> choosableColors = new ArrayList<>();
+        for (Councillor councillor: availableCouncillors)
+            if (!seen[Color.valueOf(councillor.getColor().toString()).ordinal()]) {
+                choosableColors.add(councillor.getColor());
+                seen[Color.valueOf(councillor.getColor().toString()).ordinal()]=true;
+            }
+
+        return choosableColors;
+    }
+
+    private Color chooseCouncillorColor(ArrayList<Color> choosableColors){
+
+        Message.askNewCouncillor(player);
+
+        for (int i=0; i<choosableColors.size(); i++)
+            Broker.println(i + 1 + ". " + choosableColors.get(i), player);
+
+        return choosableColors.get(inputNumber(1, choosableColors.size()));
+
+    }
+
 
 }
