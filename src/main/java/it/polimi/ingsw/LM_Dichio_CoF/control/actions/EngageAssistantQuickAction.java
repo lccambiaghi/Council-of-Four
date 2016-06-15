e56a5673821b5ecc3e0f74f3fdc14a6411f8d5b1
ale_dichio <alessio.dichio@mail.polimi.it>
2016-06-15 19:15:24 +0200

    Implementazione Engage Assistant Quick Action

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/actions/EngageAssistantQuickAction.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/actions/EngageAssistantQuickAction.java
@@ -1,10 +1,15 @@
 package it.polimi.ingsw.LM_Dichio_CoF.control.actions;
 
+import it.polimi.ingsw.LM_Dichio_CoF.control.Constant;
+import it.polimi.ingsw.LM_Dichio_CoF.control.Message;
 import it.polimi.ingsw.LM_Dichio_CoF.control.Player;
 import it.polimi.ingsw.LM_Dichio_CoF.model.Match;
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.Field;
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.Route;
 
 public class EngageAssistantQuickAction extends Action {
 
+	String resultMsg;
     public EngageAssistantQuickAction(Match match, Player player){
         this.match=match;
         this.player=player;
@@ -12,13 +17,30 @@ public class EngageAssistantQuickAction extends Action {
 
     @Override
     public boolean preliminarySteps(){
+    	Field field= match.getField();
+    	Route richnessRoute = field.getRichnessRoute();
 
-        return false;
+		if(richnessRoute.getPosition(player)<Constant.ASSISTANT_ENGAGEMENT_RICHNESS_COST){
+			Message.notEnoughRichness(player);
+			return false;
+		}
+    	
+        return true;
     }
 
     @Override
     public void execute(){
-
+    	
+    	Field field= match.getField();
+    	Route richnessRoute = field.getRichnessRoute();
+    
+    	richnessRoute.movePlayer(-(Constant.ASSISTANT_ENGAGEMENT_RICHNESS_COST),player); //eccezione lanciata qui
+		player.addAssistant(1);
+		
+		resultMsg="Player"+ player.getNickname() +"has engaged an Assistant paying three coins";
     }
+
+    @Override
+    public String getResultMsg(){return resultMsg;}
 
 }
