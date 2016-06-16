12d062dbd04d7db0ea2dce16a4023cbf0496a350
Luca Cambiaghi <luca.cambiaghi@mail.polimi.it>
2016-06-16 11:28:58 +0200

    aggiunte mosse

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/ControlMatch.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/ControlMatch.java
@@ -2,9 +2,11 @@ package it.polimi.ingsw.LM_Dichio_CoF.control;
 
 import it.polimi.ingsw.LM_Dichio_CoF.connection.Broker;
 import it.polimi.ingsw.LM_Dichio_CoF.connection.CountDown;
+import it.polimi.ingsw.LM_Dichio_CoF.control.actions.*;
 import it.polimi.ingsw.LM_Dichio_CoF.model.Market;
 import it.polimi.ingsw.LM_Dichio_CoF.model.Match;
 import it.polimi.ingsw.LM_Dichio_CoF.model.PoliticCard;
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.City;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,41 +21,15 @@ public class ControlMatch {
 	private boolean gameOver;
 
 	public ControlMatch(ArrayList<Player> arrayListPlayer){
+
 		Collections.shuffle(arrayListPlayer);
 		
 		this.players =arrayListPlayer;
 		this.match=Match.MatchFactory(arrayListPlayer);
 		this.market=match.getMarket();
+
 	}
-	
-	
-	//ONLY FOR TESTS
-	private void println(String string, Player player){
-		//FOR_TEST
-		if(Constant.test)
-			System.out.println(string);
-		else
-			Broker.println(string, player);
-	}
-	
-	private void broadcastAll(String string, ArrayList<Player> players){
-		//FOR_TEST
-		if(Constant.test)
-			System.out.println(string);
-		else
-			Broker.printlnBroadcastAll(string, players);
-	}
-	
-	private void broadcastOthers(String string, ArrayList<Player> players, Player playerNot){
-		//FOR_TEST
-		if(Constant.test)
-			System.out.println(string);
-		else
-			Broker.printlnBroadcastOthers(string, players, playerNot);
-	}
-	
-	
-	
+
 	public void startMatch(){
 
 		int turn=1;
@@ -61,7 +37,7 @@ public class ControlMatch {
 		do {
 			
 			player=players.get(turn-1);
-			
+
 			broadcastOthers("Turn of: " +player.getNickname(), players, player);
 			
 			// Draw a card
@@ -70,8 +46,7 @@ public class ControlMatch {
 			CountDown countDown = new CountDown(Constant.TIMER_SECONDS_TO_PERFORM_ACTION*1000);
 			
 			println("It's your turn! You have "+Constant.TIMER_SECONDS_TO_PERFORM_ACTION+" seconds!", player);
-			
-			
+
 			//Set actions 
 			player.setMainActionsLeft(1);
 			player.setQuickActionDone(false);
@@ -86,9 +61,9 @@ public class ControlMatch {
 				broadcastAll("The market has started!", players);
 				market.startMarket();
 				turn=1;
-			}else{
-				turn++;
 			}
+			else
+				turn++;
 			
 		}while(!gameOver);
 
@@ -97,19 +72,23 @@ public class ControlMatch {
 	public void infoOrAction(){
 
 		Message.chooseInfoOrAction_1_2(player);
+
 		choice = Broker.askInputNumber(1, 2, player);
 		
-		if(choice==1){
+		if(choice==1)
 			infoMatch();
-		}else if(choice==2){
-			ifQuickAction();
-		}
+		else
+			actionsLeftHandler();
+
 	}
 	
 	private void infoMatch(){
+
 		match.getInfoMatch().setPlayer(player);
+
 		do{
 			Message.chooseInfo_0_6(player);
+
 			choice = Broker.askInputNumber(0, 6, player);
 					
 			switch (choice) {
@@ -129,100 +108,136 @@ public class ControlMatch {
 				match.getInfoMatch().printInfoRegions(player);
 				break;
 			case 6:
-				cityFromIndex();
+				City[] arrayCity = match.getField().getArrayCity();
+				Message.chooseCity(player, arrayCity);
+				match.getInfoMatch().printInfoCity(player, Broker.askInputNumber(1, arrayCity.length, player)-1); //array positioning
 				break;
 			}
 		}
 		while(choice!=0);
+
 		infoOrAction();
+
 	}
-	
-	private void cityFromIndex(){
-		int numCities = match.getField().getArrayCity().length;
-		Message.chooseCityFromIndex(player);
-		match.getInfoMatch().printCityAndIndex(player);
-		int indexCity = Broker.askInputNumber(1, numCities, player);
-		match.getInfoMatch().printInfoCity(player, indexCity-1);
+
+	private void actionsLeftHandler(){
+
+		if(player.getMainActionsLeft()>0)
+			if(!player.isQuickActionDone())
+				ifQuickAction();
+			else
+				mainAction();
+		else
+			if(!player.isQuickActionDone())
+				ifQuickAction();
+
 	}
-	
 	
 	private void ifQuickAction(){
+
 		Message.ifQuickAction(player);
 		Message.chooseYesOrNo_1_2(player);
+
 		choice = Broker.askInputNumber(1, 2, player);
-		if(choice==1){
+
+		if(choice==1)
 			quickAction();
-		}else if(choice==2){
+		else
 			if(player.getMainActionsLeft()!=0)
 				mainAction();
-		}
+
 	}
 	
 	
-	private void quickAction() { 
+	private void quickAction() {
+
 		Message.chooseQuickAction_1_4(player);
+
 		choice = Broker.askInputNumber(1, 4, player);
+
+		Action action;
+
 		switch (choice) {
 			case 1:
-				//engageAssistant(player);
+				action= new EngageAssistantQuickAction(match, player);
 				break;
 			case 2:
-				//replacePermitCards(player);
+				action= new ChangePermitCardsQuickAction(match, player);
 				break;
 			case 3:
-				//electCouncillorWithAssistant(player);
+				action= new ElectCouncillorWithAssistantQuickAction(match,player);
 				break;
-			case 4:
-				//performAdditionalMainAction(player);
+			default:
+				action= new PerformAdditionalMAQuickAction(match,player);
 				break;
 		}
-		
-		//IF QUICK ACTION DONE SUCCESSFULLY
+
+		if (action.preliminarySteps()) {
+			action.execute();
 			player.setQuickActionDone(true);
+		}
 		
 		actionsLeftHandler();
 			
 	}
 	
 	private void mainAction() {
+
 		Message.chooseMainAction_1_4(player);
+
 		choice = Broker.askInputNumber(1, 4, player);
+
+		Action action;
+
 		switch (choice) {
 			case 1:
-				//electCouncillor(playerTurn);
+				action = new ElectCouncillorMainAction(match, player);
 				break;
 			case 2:
-				//acquirePermitCard(playerTurn);
+				action = new AcquirePermitCardMainAction(match, player);
 				break;
 			case 3:
-				//buildEmporiumWithPermitCard(playerTurn);
+				action = new BuildEmporiumPermitCardMainAction(match, player);
 				break;
-			case 4:
-				//buildEmporiumWithKing(playerTurn);
+			default:
+				action = new BuildEmporiumWithKingMainAction(match, player);
 				break;
 		}
 		
-		//IF MAIN ACTION DONE SUCCESSFULLY
+		if (action.preliminarySteps()) {
+			action.execute();
 			player.setMainActionsLeft(player.getMainActionsLeft() - 1);
-		
+		}
+
 		actionsLeftHandler();
 		
 	}
-
-	private void actionsLeftHandler(){
-		if(player.getMainActionsLeft()>0){
-			if(!player.isQuickActionDone()){
-				ifQuickAction();
-			}else{
-				mainAction();
-			}
-		}else if(player.getMainActionsLeft()==0){
-			if(!player.isQuickActionDone()){
-				ifQuickAction();
-			}
-		}
-	}
 	
 	public void setGameOver(){ gameOver=true;	}
+
+	//ONLY FOR TESTS
+	private void println(String string, Player player){
+		//FOR_TEST
+		if(Constant.test)
+			System.out.println(string);
+		else
+			Broker.println(string, player);
+	}
+
+	private void broadcastAll(String string, ArrayList<Player> players){
+		//FOR_TEST
+		if(Constant.test)
+			System.out.println(string);
+		else
+			Broker.printlnBroadcastAll(string, players);
+	}
+
+	private void broadcastOthers(String string, ArrayList<Player> players, Player playerNot){
+		//FOR_TEST
+		if(Constant.test)
+			System.out.println(string);
+		else
+			Broker.printlnBroadcastOthers(string, players, playerNot);
+	}
 
 }
