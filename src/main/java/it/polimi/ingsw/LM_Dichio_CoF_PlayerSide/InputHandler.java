a085b8b0a8f7e3b3221819cd55f51e98eb5a4428
ale_dichio <alessio.dichio@mail.polimi.it>
2016-06-25 12:14:47 +0200

    aggiunto e modificato inputCity

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/InputHandler.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF_PlayerSide/InputHandler.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.LM_Dichio_CoF_PlayerSide;
 
 import java.util.Scanner;
 
+import it.polimi.ingsw.LM_Dichio_CoF.model.field.CityName;
+
 public class InputHandler {
 
 	public static int inputNumber(int lowerBound, int upperBound){ //TODO throws RemoteException + spostare nella classe della CLI
@@ -27,5 +29,35 @@ public class InputHandler {
 
 		return inputNumber;
 
+	}
+	public static String inputCity (CityName currentCity, CityName lastCity){
+		Scanner in = new Scanner(System.in);
+		String string;
+		boolean correct=true;
+		do{
+			string = in.nextLine();
+			string.replaceAll("\\s", "");
+			
+			for(int i=0,j=lastCity.ordinal();i<string.length() && correct;i++){
+				int enumPosition=CityName.valueOf(String.valueOf(string.charAt(i))).ordinal();
+				for(int k=1; i<k && k<string.length() && correct; k++){
+					if(string.charAt(i)==string.charAt(k)){
+						System.out.println("You have inserted the same city more than once.");
+						System.out.println("Please insert another set of cities");
+						correct=false;
+					}
+				}
+				if (enumPosition> j){
+					System.out.println("You have inserted a city out of range");
+					System.out.println("Please insert another set of cities");
+					correct=false;
+				}
+				if (enumPosition==currentCity.ordinal()){
+					System.out.println("You have inserted the same city of the started.");
+					System.out.println("Please insert another set of cities");
+				}
+			}
+		}while(!correct);
+		return string;
 	}
 }
