00633edac3a86e8a1f977621b354cb8e2137c416
ale_dichio <alessio.dichio@mail.polimi.it>
2016-07-03 23:16:54 +0200

    Modificata la classe Selling Object in una classe astratta

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/model/field/SellingObject.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/model/field/SellingObject.java
@@ -3,28 +3,19 @@ package it.polimi.ingsw.LM_Dichio_CoF.model.field;
 import it.polimi.ingsw.LM_Dichio_CoF.control.Player;
 
 
-public class SellingObject {
-	private int price;
-	private Object object;
-	private Player owner;
-	
-	public SellingObject (Player player, Object object, int price){
-		
-		this.object=object;
-		this.price=price;
-		this.owner=player;
-	}
+public abstract class SellingObject {
+	public int price;
+	public Player owner;
 	
 	public int getPrice() {
 		return price;
 	}
 
-	public Object getObject() {
-		return object;
-	}
-
 	public Player getOwner() {
 		return owner;
 	}
-
+	
+	public abstract String getObjectInfo();
+	
+	public abstract void addToPlayer(Player buyer);
 }
