0f4ad9e3858f7d6b2eced9981322455cdc9c1f88
ale_dichio <alessio.dichio@mail.polimi.it>
2016-07-05 23:28:04 +0200

    tolto gli rgb

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/model/field/CityColor.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/model/field/CityColor.java
@@ -4,19 +4,12 @@ import java.util.Random;
 
 public enum CityColor {
 
-    Red("255, 0, 0"), // Red solo per numberCities=21
-    Purple("88, 86, 214"),
-    Blue("0, 0, 255"),
-    Bronze("205, 127, 50"),
-    Gold("255, 215, 0"),
-    Silver("192, 192, 192");
-
-    private String rgb;
-
-    /* The constructor assigns the rgb passed*/
-    CityColor(String rgb) {
-        this.rgb=rgb;
-    }
+    Red,
+    Purple,
+    Blue,
+    Bronze,
+    Gold,
+    Silver;
 
     public static CityColor getCityColorFromIndex(int index){ return CityColor.values()[index]; }
 
@@ -24,19 +17,4 @@ public enum CityColor {
         Random random = new Random();
         return values()[random.nextInt(values().length)];
     }
-
-    public String getRgb() {
-        return rgb;
-    }
-    public void setRgb(String rgb) {
-        this.rgb = rgb;
-    }
-
-    /*
-    public static CityColor getRandomCityColorWithoutRed(){
-        Random random = new Random();
-        return values()[random.nextInt(values().length-1)];
-    }
-    */
-
 }
