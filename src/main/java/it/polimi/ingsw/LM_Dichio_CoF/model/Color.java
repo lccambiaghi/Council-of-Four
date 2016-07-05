89c7dd392acc134fabae83c908ebfce40829f783
ale_dichio <alessio.dichio@mail.polimi.it>
2016-07-05 23:26:44 +0200

    eliminati gli rgb

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/model/Color.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/model/Color.java
@@ -3,18 +3,15 @@ import java.util.Random;
 
 public enum Color {
 	
-	White("255, 255, 255"),
-	Black("0, 0, 0"), 
-	Cyan ("0, 128, 255"), 
-	Orange ("255, 128, 0"), 
-	Pink("255, 153, 255"), 
-	Purple("88, 86, 214"),
-	Multicolor("Multicolor"); //gestisci poi multicolor
+	White,
+	Black, 
+	Cyan, 
+	Orange, 
+	Pink, 
+	Purple,
+	Multicolor;
 	
-	public String rgb;
-
 	/* The constructor assigns rgb*/
-	Color(String rgb) { this.rgb=rgb; }
 
 	public static Color getRandomColor(){
 		Random random = new Random ();
@@ -23,13 +20,6 @@ public enum Color {
 	
 	public static Color getColorFromIndex(int index){
 		return Color.values()[index];
-	}
-
-	public String getRgb() {
-		return rgb;
-	}
-	public void setRgb(String rgb) {
-		this.rgb = rgb;
 	}
 
 }
