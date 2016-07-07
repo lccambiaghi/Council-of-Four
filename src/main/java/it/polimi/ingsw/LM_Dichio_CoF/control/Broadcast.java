8df4bdc4ce16d1438ee7e5ceb438246245941550
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-07-07 18:06:55 +0200

    Gestisce la disconnessione da solo

--- src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/Broadcast.java
+++ src/main/java/it/polimi/ingsw/LM_Dichio_CoF/control/Broadcast.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 
 public class Broadcast {
 
-	public static void printlnBroadcastAll(String string, ArrayList<Player> players) throws InterruptedException{
+	public static void printlnBroadcastAll(String string, ArrayList<Player> players){
 		for(Player p: players){
-			p.getBroker().println(string);
+			try {
+				p.getBroker().println(string);
+			} catch (InterruptedException e) {}
 		}
 	}
 	
-	public static void printlnBroadcastOthers(String string,  ArrayList<Player> players, Player playerNot) throws InterruptedException{
+	public static void printlnBroadcastOthers(String string,  ArrayList<Player> players, Player playerNot){
 		for(Player p: players){
 			if(!p.equals(playerNot))
-				p.getBroker().println(string);
+				try {
+					p.getBroker().println(string);
+				} catch (InterruptedException e) {}
 		}
 	}
 }
