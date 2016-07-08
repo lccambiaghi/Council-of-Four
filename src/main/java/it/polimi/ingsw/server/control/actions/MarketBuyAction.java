9368a4a919a9473d3f44841c04313fc717d9c36a
ale_dichio <alessio.dichio@mail.polimi.it>
2016-07-08 17:29:01 +0200

    Create le actions per la fase di vendita e acquisto del market

---
+++ src/main/java/it/polimi/ingsw/server/control/actions/MarketBuyAction.java
@@ -0,0 +1,115 @@
+package it.polimi.ingsw.server.control.actions;
+
+import java.util.ArrayList;
+
+import it.polimi.ingsw.server.control.Message;
+import it.polimi.ingsw.server.control.Player;
+import it.polimi.ingsw.server.model.Match;
+import it.polimi.ingsw.server.model.SellingObject;
+
+public class MarketBuyAction extends Action {
+	 
+    public SellingObject possibleBuyedObject;
+    public int choosenObject;
+    public ArrayList <SellingObject> arrayListSellingObjects;
+   
+    public MarketBuyAction (Match match, Player player){
+        this.match=match;
+        this.player=player;
+    }
+   
+    /**
+     * This is the method used to buy an object. When the selling phase is finished
+     * start this method and first of all it clone the arrayList of players and shuffles it,
+     * because we need random turns. Then it ask to the first player if he wants to buy something
+     * and if the answer is positive, it shows all the objects that are in the arraylist.
+     * The player can choose one of them or quit the market. If it hasn't money and he cannot
+     * buy nothing, he can presses 0 and quits the buying phase.
+     * @param arrayListSelingObjects
+     * @throws InterruptedException
+     */
+     
+    @Override
+    public boolean preliminarySteps() throws InterruptedException{
+        boolean buyingSuccessful;
+        arrayListSellingObjects = match.getMarket().getArrayListSellingObjects();
+ 
+        player.getBroker().println(Message.chooseToBuySomething_1_2());
+        if(player.getBroker().askInputNumber(1,2)==1){
+            player.getBroker().println(Message.askObjectToBuy());
+            player.getBroker().println(Message.skipBuying());
+            for (int i=0; i<arrayListSellingObjects.size();i++){
+                player.getBroker().println((i+1)+ ". " + arrayListSellingObjects.get(i).getObjectInfo());
+            }
+            do{
+                choosenObject = player.getBroker().askInputNumber(0, arrayListSellingObjects.size())-1;
+                if(choosenObject==-1){
+                    buyingSuccessful=true;
+                    return false;
+                }              
+                else{
+                    possibleBuyedObject=arrayListSellingObjects.get(choosenObject);
+                   
+                    if (canBuy(possibleBuyedObject.getPrice(), player)){
+                        buyingSuccessful=true;
+                    }
+                    else {
+                        buyingSuccessful=false;
+                        player.getBroker().println(Message.askObjectToBuy());
+                        player.getBroker().println(Message.skipBuying());
+                    }
+                }
+            }
+            while(!buyingSuccessful);
+            return true;
+        }
+        else
+            return false;
+    }
+   
+    /**
+     * The method verify if the buyer has the sufficient richness to buy the object, then
+     * it provides to set the new values of the richness for the buyer and the seller.
+     * @param price
+     * @param playerTurn: the buyer
+     * @param sellingPlayer
+     * @return true if he has sufficient money, false otherwise
+     * @throws InterruptedException
+     */
+    private boolean canBuy(int price, Player playerTurn) throws InterruptedException{
+        if(checkIfEnoughRichness(playerTurn, price)){
+            return true;
+        }
+        playerTurn.getBroker().println(Message.deniedBuying());
+        return false;
+    }
+   
+    private boolean checkIfEnoughRichness(Player playerTurn, int payed) {
+   
+        if (playerTurn.getRichness()<payed)
+            return false;
+        else
+            return true;
+ 
+}
+   
+    @Override
+    public void execute(){ 
+        Player sellingPlayer = possibleBuyedObject.getOwner();
+        int price = possibleBuyedObject.getPrice();
+       
+        possibleBuyedObject.addToPlayer(player);
+        arrayListSellingObjects.remove(choosenObject);
+       
+        sellingPlayer.addRichness(price);
+        player.decrementRichness(price);
+       
+        resultMsg="Player " + player.getNickname() + " buyed an object from the market";
+	       
+	    }
+	   
+	  @Override	
+	  public String getResultMsg(){return resultMsg;}
+	
+}
+
---
+++ src/main/java/it/polimi/ingsw/server/control/actions/MarketSellAction.java
@@ -0,0 +1,198 @@
+package it.polimi.ingsw.server.control.actions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import it.polimi.ingsw.server.control.MarketHandler;
+import it.polimi.ingsw.server.control.Message;
+import it.polimi.ingsw.server.control.Player;
+import it.polimi.ingsw.server.model.Match;
+import it.polimi.ingsw.server.model.PermitCard;
+import it.polimi.ingsw.server.model.PoliticCard;
+import it.polimi.ingsw.server.model.SellingAssistants;
+import it.polimi.ingsw.server.model.SellingObject;
+import it.polimi.ingsw.server.model.SellingPermitCard;
+import it.polimi.ingsw.server.model.SellingPoliticCard;
+
+public class MarketSellAction extends Action{
+	 
+    private SellingObject sellingObject;
+   
+    public MarketSellAction (Match match, Player player){
+            this.match=match;
+            this.player=player;
+        }
+   
+    @Override
+    public boolean preliminarySteps() throws InterruptedException{
+        int choice;
+ 
+        boolean [] sellable = new boolean [3] ;
+        int numberPermitCard=player.getArrayListPermitCard().size();
+        int numberPoliticCard=player.getArrayListPoliticCard().size();
+        int numberAssistants=player.getAssistant();
+        int [] stock = new int [3];
+        stock[0]=numberPermitCard;
+        stock[1]=numberPoliticCard;
+        stock[2]=numberAssistants;
+        Map <Integer, String> itemsMenu = new HashMap<>();
+ 
+        int counter=1;
+        if (numberPermitCard>0 || numberPoliticCard>0 || numberAssistants>0){
+           
+            player.getBroker().println(Message.chooseToSellSomething_1_2());
+            if(player.getBroker().askInputNumber(1,2)==1){
+                player.getBroker().println(Message.askForObject());
+               
+                for(int i=0; i<stock.length; i++){
+                    if(stock[i]>0)
+                        sellable[i]=true;
+                }
+                for(int i=0; i<sellable.length;i++){
+                    switch(i){
+                        case 0:
+                            if(sellable[i]){
+                                player.getBroker().println(Message.permitCard(counter));
+                                itemsMenu.put(counter, "PermitCard");
+                                counter++;
+                            }
+                            break;
+                        case 1:
+                            if(sellable[i]){
+                                player.getBroker().println(Message.politicCard(counter));
+                                itemsMenu.put(counter, "PoliticCard");
+                                counter++;
+                            }
+                            break;
+                        case 2:
+                            if(sellable[i]){
+                                player.getBroker().println(Message.assistants(counter));
+                                itemsMenu.put(counter, "Assistants");
+                            }
+                            break;
+                    }
+                }
+                choice= player.getBroker().askInputNumber(1, counter);
+                selectObject(player, itemsMenu.get(choice));
+            }
+        }else{
+            player.getBroker().println(Message.deniedSelling());
+            return false;
+        }
+        return true;
+    }
+   
+    /**
+     * This method read the value inserted by the player and call the right method to
+     * ask to the player the physical object. When it has finished the object is added
+     * to the arraylist of selling object
+     * @param playerTurn
+     * @param type: it could be "PoliticCard", "PermitCard" or "Assistants"
+     * @throws InterruptedException
+     */
+   
+    private void selectObject(Player player, String type) throws InterruptedException{ 
+       
+        PermitCard chosenPermitCard;
+        PoliticCard chosenPoliticCard;
+        int chosenAssistants;
+        int price;
+       
+        switch(type){
+        case "PermitCard":
+            chosenPermitCard=askPermitCard(player);
+            price = askPrice(player);
+            sellingObject = new SellingPermitCard (chosenPermitCard, player, price);
+            break;
+        case "PoliticCard":
+            chosenPoliticCard = askPoliticCard (player);
+            price = askPrice(player);
+            sellingObject=new SellingPoliticCard(chosenPoliticCard, player, price);
+            break;
+        case "Assistants":
+            chosenAssistants = askAssistant (player);
+            price = askPrice(player);
+            sellingObject=new SellingAssistants(chosenAssistants, player, price);
+            break;
+        }
+       
+    }
+    /**
+     * The method askPoliticCard iterates on the Politic Cards of the user and he has to select
+     * a value from an lowerbound to an upperbound. After that he has to choose the price of the
+     * object.  
+     * @param playerTurn
+     * @return
+     * @throws InterruptedException
+     */
+   
+    private PoliticCard askPoliticCard(Player playerTurn) throws InterruptedException{
+        ArrayList <PoliticCard> playerPoliticCards = playerTurn.getArrayListPoliticCard();
+       
+        playerTurn.getBroker().println(Message.choosePoliticCard(playerPoliticCards));
+       
+        int chosenPoliticCard= playerTurn.getBroker().askInputNumber (1, playerPoliticCards.size())-1;
+       
+        PoliticCard sellingPoliticCard = playerTurn.getArrayListPoliticCard().get(chosenPoliticCard);
+        playerTurn.getArrayListPoliticCard().remove(chosenPoliticCard);
+        return sellingPoliticCard;
+    }
+    /**
+     *
+     * The method askPermitCard iterates on the Politic Cards of the user and he has to select
+     * a value from an lowerbound to an upperbound. After that he has to choose the price of the
+     * object.  
+     * @param playerTurn
+     * @return
+     * @throws InterruptedException
+     */
+    private PermitCard askPermitCard(Player playerTurn) throws InterruptedException{
+        ArrayList <PermitCard> playerPermitCards = playerTurn.getArrayListPermitCard();
+       
+        playerTurn.getBroker().println(Message.choosePermitCardNoBonus(playerPermitCards));
+ 
+        int chosenPermitCard = playerTurn.getBroker().askInputNumber(1, playerPermitCards.size())-1;
+       
+        PermitCard sellingPermitCard = playerTurn.getArrayListPermitCard().get(chosenPermitCard);
+        playerTurn.getArrayListPermitCard().remove(chosenPermitCard);
+       
+        return sellingPermitCard;
+    }
+    /**
+     * The method askAssistants shows to the player how many assistants he has; then the player
+     * has to insert a number of the assistants that he would like to sell.
+     * After that he has to choose the price of them.
+     *  
+     * @param playerTurn
+     * @return
+     * @throws InterruptedException
+     */
+    private int askAssistant(Player playerTurn) throws InterruptedException{
+        playerTurn.getBroker().println(Message.chooseAssistants(playerTurn));
+   
+        int chosenAssistants= playerTurn.getBroker().askInputNumber(1, playerTurn.getAssistant());
+       
+        playerTurn.decrementAssistant(chosenAssistants);
+        return chosenAssistants;
+    }
+   
+    private int askPrice(Player playerTurn) throws InterruptedException{
+        playerTurn.getBroker().println(Message.askPrice());
+        return playerTurn.getBroker().askInputNumber(1, 20);
+    }
+   
+    @Override
+    public void execute(){
+        MarketHandler market = match.getMarket();
+       
+        market.addArrayListSellingObject(sellingObject);
+       
+        resultMsg="Player " + player.getNickname() + " added a new object in the market";
+       
+    }
+   
+    @Override
+    public String getResultMsg(){return resultMsg;}
+   
+}
