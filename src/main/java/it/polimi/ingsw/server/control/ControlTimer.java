ce0152ae6727bf5ef5990efc361a08f6650ea343
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-07-08 20:33:21 +0200

    ControlTimer offre metodi per la gestione dei timer, molto efficiente

--- src/main/java/it/polimi/ingsw/server/control/ControlTimer.java
+++ src/main/java/it/polimi/ingsw/server/control/ControlTimer.java
@@ -3,13 +3,19 @@ package it.polimi.ingsw.server.control;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class CountDown {
+public class ControlTimer {
 
-	Timer timer;
-	boolean finished = false;
+	private Timer timer;
+	private boolean finished = false;
+	
+	public ControlTimer(){}
 	
 	
-	public CountDown(int seconds){
+	/**
+	 * 
+	 * @param seconds
+	 */
+	public void startCountDown(int seconds){
 		timer = new Timer();
 		timer.schedule(new TimeIsFinished(), seconds*1000);
 	}
@@ -21,6 +27,33 @@ public class CountDown {
 		}
 	}
 	
-	public boolean isTimeFinished(){ return finished; }
+	public boolean isCountDownFinished(){ return finished; }
+	
+	
+	/**
+	 * 
+	 * @param t
+	 * @param seconds
+	 */
+	public void waitForThreadUntilTimerExpires(Thread t, int seconds){
+		/**
+		 * This "while" permits to check every second if the timer
+		 * to set configurations has expired
+		 */
+		long startTime = System.currentTimeMillis();
+		long endTime = startTime + (seconds)*1000;
+		while (System.currentTimeMillis() < endTime) {
+		    try {
+		         Thread.sleep(1000);  // Sleep 1 second
+		         
+		         // If the player has set players before the timer expires
+		         if(!t.isAlive())
+		        	 break;
+		         
+		    } catch (InterruptedException e) {}	
+		}
+	}
+	
+	
 	
 }
