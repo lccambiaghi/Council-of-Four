010a058a94ce203c2cd1071a2dba2f1c77b96b5a
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-07-10 20:00:36 +0200

    JavaDoc Broadcast

--- src/main/java/it/polimi/ingsw/server/control/Broadcast.java
+++ src/main/java/it/polimi/ingsw/server/control/Broadcast.java
@@ -2,14 +2,30 @@ package it.polimi.ingsw.server.control;
 
 import java.util.ArrayList;
 
+/**
+ * This class offers the methods to print a specific string in the console of more players (clients)
+ * 
+ * printlnBroadcastAll : println the specific string in all players console
+ * 
+ * printlnBroadcastOthers : println the specific string in all players console except the "playerNot"
+ */
 public class Broadcast {
 
+	/**
+	 * @param string to be printed 
+	 * @param players where print the string
+	 */
 	public static void printlnBroadcastAll(String string, ArrayList<Player> players){
 		for(Player p: players){
 			p.getBroker().println(string);
 		}
 	}
 	
+	/**
+	 * @param string to be printed 
+	 * @param players where print the string
+	 * @param playerNot that belongs to "players" where not print the string
+	 */
 	public static void printlnBroadcastOthers(String string,  ArrayList<Player> players, Player playerNot){
 		for(Player p: players){
 			if(!p.equals(playerNot))
