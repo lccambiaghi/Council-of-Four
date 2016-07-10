833fb69191709706a7a098abeedbc796b4bc7ff2
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-07-10 17:57:48 +0200

    JavaDoc DisconnectedException

--- src/main/java/it/polimi/ingsw/utils/DisconnectedException.java
+++ src/main/java/it/polimi/ingsw/utils/DisconnectedException.java
@@ -1,13 +1,15 @@
 package it.polimi.ingsw.utils;
 
+/**
+ * This exception is thrown when a player disconnects
+ */
 public class DisconnectedException extends Exception {
 
+	/**
+	 * Construct a new DisconnectedException
+	 */
 	public DisconnectedException(){
 		super();
-	}
-	
-	public DisconnectedException(String s){
-		super(s);
 	}
 	
 }
