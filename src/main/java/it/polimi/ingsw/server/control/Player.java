85697a24129b31da90d275d402200710ba0ddb8a
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2016-07-10 19:22:53 +0200

    JavaDoc del Player completata

--- src/main/java/it/polimi/ingsw/server/control/Player.java
+++ src/main/java/it/polimi/ingsw/server/control/Player.java
@@ -8,42 +8,55 @@ import it.polimi.ingsw.server.model.PermitCard;
 
 import java.util.ArrayList;
 
-
+/**
+ * This class represents the client connected to the server and offers both the methods of the connection
+ * and the ones of the match.
+ */
 public class Player{
 	
-	private String nickname;
+	
+	private ArrayList <PoliticCard> arrayListPoliticCard= new ArrayList<>();
+	private ArrayList <PermitCard> arrayListPermitCard = new ArrayList<>();
+	private ArrayList <PermitCard> arrayListUsedPermitCard = new ArrayList<>();
+	private ArrayList <City>  arrayListEmporiumBuilt = new ArrayList<>();
+	
+	
+	
+	// ************ CONNECTION VARIABLES ************ //
 
+	private String nickname;
 	private Broker broker;
 	
-	// variables of the game
 	private boolean connected = true;
 	private boolean logged = false;
 	private boolean messageDisconnectedSent = false;
-	private boolean lastTurnDone;
+	
+	// ********************************************** //
 
+	
+	
+	// ************ MATCH VARIABLES ************ //
+	
 	private int richness;
 	private int assistant;
 	private int victory;
-	
-	private ArrayList <PoliticCard> arrayListPoliticCard= new ArrayList<>();
-
-	private ArrayList <PermitCard> arrayListPermitCard = new ArrayList<>();
-	private ArrayList <PermitCard> arrayListUsedPermitCard = new ArrayList<>();
 
 	private int mainActionsLeft;
 	private boolean quickActionDone;
-
-	private ArrayList <City>  arrayListEmporiumBuilt = new ArrayList<>();
-
-
-	/* Constructor */
-	public Player(){}
 	
-	public Broker getBroker() {return broker;}
-	public void setBroker(Broker broker) {this.broker=broker;}
+	private boolean lastTurnDone;
+
+	// ***************************************** //
+	
+	
+	
+	// ************ CONNECTION METHODS ************ //
 
 	public String getNickname() {return nickname;}
 	public void setNickname(String nickname) {this.nickname = nickname;}
+	
+	public Broker getBroker() {return broker;}
+	public void setBroker(Broker broker) {this.broker=broker;}
 	
 	public boolean isConnected() {return connected;}
 	public void setConnected(boolean connected) {this.connected = connected;}
@@ -54,8 +67,31 @@ public class Player{
 	public boolean isMessageDisconnectedSent() {return messageDisconnectedSent;}
 	public void setMessageDisconnectedSent(boolean messageDisconnectedSent) {this.messageDisconnectedSent = messageDisconnectedSent;	}
 
+	// ******************************************** //
+	
+	
+	
+	// ************ MATCH METHODS ************ //
+	
+	// VICTORY 
+	
+	public int getVictory() {return victory;}
+	
+	/**
+	 * @param increment to be applied
+	 */
+	public void addVictory(int increment){victory+=increment;}
+	
+	
+	// RICHNESS
+	
 	public int getRichness() {return richness;}
+	
 	public void setRichness(int richness) {this.richness = richness;}
+	
+	/**
+	 * @param increment to be applied
+	 */
 	public void addRichness(int increment){
 		if (this.getRichness()+increment>Constant.RICHNESS_MAX){
 			this.setRichness(Constant.RICHNESS_MAX);
@@ -63,49 +99,100 @@ public class Player{
 		else
 			this.setRichness(this.getRichness()+increment);
 	}
+	
+	/**
+	 * @param decrement to be applied (decrement>0)
+	 */
 	public void decrementRichness(int decrement) {
 		this.setRichness(this.getRichness()-decrement);
 	}
 	
+	
+	// ASSISTANTS
+	
 	public int getAssistant() {	return assistant;}
 	public void setAssistant(int assistant) {this.assistant = assistant;}
-	public void addAssistant(int increment)	{assistant +=increment;}
-	public void decrementAssistant(int decrement)	{assistant -=decrement;}
 	
-	public void addVictory(int increment){victory+=increment;}
+	/**
+	 * @param increment to be applied
+	 */
+	public void addAssistant(int increment)	{assistant +=increment;}
+	
+	/**
+	 * @param decrement to be applied (decrement>0)
+	 */
+	public void decrementAssistant(int decrement)	{assistant -=decrement;}
 
+	
+	// POLITICS CARDS
+	
 	public ArrayList<PoliticCard> getArrayListPoliticCard() {return arrayListPoliticCard;}
+	
+	/**
+	 * @param politicCard to be applied
+	 */
 	public void addPoliticCard(PoliticCard politicCard) {arrayListPoliticCard.add(politicCard);}
+	
+	/**
+	 * @param politicCard used
+	 */
 	public void discardPoliticCard(PoliticCard politicCard){arrayListPoliticCard.remove(politicCard);}
 
+	
+	// PERMIT CARDS
+	
 	public ArrayList<PermitCard> getArrayListPermitCard(){return arrayListPermitCard;}
 	public ArrayList<PermitCard> getArrayListUsedPermitCard(){return arrayListUsedPermitCard;}
+	
+	/**
+	 * @param permitCard to be applied
+	 */
 	public void acquirePermitCard(PermitCard permitCard){arrayListPermitCard.add(permitCard);}
+	
+	/**
+	 * @param permitCard to be used
+	 */
 	public void usePermitCard (PermitCard permitCard){
 		int i=arrayListPermitCard.indexOf(permitCard);
 		arrayListUsedPermitCard.add(arrayListPermitCard.remove(i));
 	}
 	
+	
+	// EMPORIUMS BUILT
+	
 	public ArrayList<City> getArrayListEmporiumBuilt(){return arrayListEmporiumBuilt;}
+	
+	/**
+	 * @param city where an emporium has been built
+	 */
 	public void addCityEmporiumBuilt (City city){arrayListEmporiumBuilt.add(city);}
 
+	
+	// ACTIONS
+	
 	public int getMainActionsLeft() {return mainActionsLeft;}
 	public void setMainActionsLeft(int mainActionsLeft) {this.mainActionsLeft=mainActionsLeft;}
+	
+	/**
+	 * @param increment to be applied
+	 */
 	public void addMainActionLeft(int increment) {mainActionsLeft += increment;}
+	
+	/**
+	 * @param decrement to be applied (decrement>0)
+	 */
 	public void decrementMainActionLeft(int decrement) {mainActionsLeft -= decrement;}
 
 	public boolean isQuickActionDone() {return quickActionDone;}
 	public void setQuickActionDone(boolean quickActionDone) {this.quickActionDone = quickActionDone;}
 
-	public int getVictory() {
-		return victory;
-	}
+	
+	// TURN
+	
+	public boolean isLastTurnDone() {return lastTurnDone;}
+	public void setLastTurnDone(boolean lastTurnDone) {this.lastTurnDone = lastTurnDone;}
+	
 
-	public boolean isLastTurnDone() {
-		return lastTurnDone;
-	}
-	public void setLastTurnDone(boolean lastTurnDone) {
-		this.lastTurnDone = lastTurnDone;
-	}
-
+	// *************************************** //
+	
 }
