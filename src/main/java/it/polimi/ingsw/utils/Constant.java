9a88ce4f7291fa6d6a711c8310282d711be31470
Luca Cambiaghi <luca.cambiaghi@mail.polimi.it>
2016-07-11 11:21:41 +0200

    Final changes to constant

--- src/main/java/it/polimi/ingsw/utils/Constant.java
+++ src/main/java/it/polimi/ingsw/utils/Constant.java
@@ -22,15 +22,12 @@ public class Constant {
 	
 	//file configurations
 	public static final String PATH_FILE_CONFIGURATIONS = "./src/configurations/config";
-
-	public static final int PLAYERS_MIN_NUMBER = 2;
-	public static final int PLAYERS_MAX_NUMBER = 8;
 	
 	//timers
-	public static final int TIMER_SECONDS_TO_PERFORM_ACTION = 20; //TODO change it back
+	public static final int TIMER_SECONDS_TO_PERFORM_ACTION = 60;
 	public static final int TIMER_SECONDS_NEW_MATCH = 20; 
-	public static final int TIMER_SECONDS_WAITING_CONFIGURATIONS = 10;
-	public static final int TIMER_SECONDS_MARKET_ACTION = 10;
+	public static final int TIMER_SECONDS_WAITING_CONFIGURATIONS = 15;
+	public static final int TIMER_SECONDS_MARKET_ACTION = 20;
 
 	//assumptions
 	public static final int CITIES_NUMBER_LOW = 15;
@@ -40,25 +37,28 @@ public class Constant {
 	//board game fixed parameters
 	public static final int REGIONS_NUMBER =3;
 	public static final int BALCONIES_NUMBER=4;
-	public static final int COLORS_NUMBER =6; // COUNCILORS
+	public static final int COLORS_NUMBER =6;
 	public static final int FACE_UP_PERMIT_CARD_PER_REGION_NUMBER =2;
 	public static final int COUNCILLORS_PER_BALCONY_NUMBER =4;
 
 	//board game easily changeable parameters
-	public static final int COUNCILLORS_NUMBER_TOTAL =24; //IT HAS TO BE A MULTIPLE OF THREE
+	public static final int PLAYERS_MIN_NUMBER = 2;
+	public static final int PLAYERS_MAX_NUMBER = 8;
 	public static final int POLITIC_CARDS_INITIAL_NUMBER =6;
+	public static final int COUNCILLORS_NUMBER_TOTAL =24; 	//IT HAS TO BE A MULTIPLE OF THREE
 	public static final int ASSISTANT_INITIAL_FIRST_PLAYER = 1;
 	public static final int RICHNESS_INITIAL_FIRST_PLAYER =10;
 	public static final int RICHNESS_MAX =20;
 	public static final int NOBILITY_MAX =20;
 	public static final CityName KING_CITY_INITIAL = CityName.J;
-	public static final int REGION_TILE_VICTORY_INCREMENT = 5;
-	public static final int BLUE_BONUS_TILE_VICTORY_INCREMENT = 5;
-	public static final int RED_BONUS_TILE_VICTORY_INCREMENT = 7;
-	public static final int BRONZE_BONUS_TILE_VICTORY_INCREMENT = 8;
-	public static final int SILVER_BONUS_TILE_VICTORY_INCREMENT = 12;
-	public static final int GOLD_BONUS_TILE_VICTORY_INCREMENT = 20;
 	public static final int NUMBER_EMPORIUMS_TO_WIN = 10;
+
+	//moves cost
+	public static final int PERMIT_CARD_CHANGE_ASSISTANT_COST = 1;
+	public static final int ELECTION_RICHNESS_INCREMENT = 4;
+	public static final int ASSISTANT_ENGAGEMENT_RICHNESS_COST = 3;
+	public static final int ELECTION_ASSISTANT_COST = 1;
+	public static final int ADDITIONAL_MAIN_MOVE_ASSISTANT_COST = 3;
 
 	//reward tiles' increments
 	public static final int FIRST_KING_REWARD_VICTORY_INCREMENT = 25;
@@ -66,6 +66,12 @@ public class Constant {
 	public static final int THIRD_KING_REWARD_VICTORY_INCREMENT = 12;
 	public static final int FOURTH_KING_REWARD_VICTORY_INCREMENT = 7;
 	public static final int FIFTH_KING_REWARD_VICTORY_INCREMENT = 3;
+	public static final int REGION_TILE_VICTORY_INCREMENT = 5;
+	public static final int BLUE_BONUS_TILE_VICTORY_INCREMENT = 5;
+	public static final int RED_BONUS_TILE_VICTORY_INCREMENT = 7;
+	public static final int BRONZE_BONUS_TILE_VICTORY_INCREMENT = 8;
+	public static final int SILVER_BONUS_TILE_VICTORY_INCREMENT = 12;
+	public static final int GOLD_BONUS_TILE_VICTORY_INCREMENT = 20;
 
 	//end of the game's increments
 	public static final int VICTORY_INCREMENT_LAST_EMPORIUM = 3;
@@ -77,14 +83,7 @@ public class Constant {
 	public static final int PERMIT_CARD_CITIES_NUMBER_MAX =3;
 	public static final int PERMIT_CARD_CITIES_NUMBER_MIN =1;
 	public static final int CITIES_PER_REGION_MIN =5;
-	public static final int INITIALLY_KING_CITY = 9;
-
-	//moves cost
-	public static final int PERMIT_CARD_CHANGE_ASSISTANT_COST = 1;
-	public static final int ELECTION_RICHNESS_INCREMENT = 4;
-	public static final int ASSISTANT_ENGAGEMENT_RICHNESS_COST = 3;
-	public static final int ELECTION_ASSISTANT_COST = 1;
-	public static final int ADDITIONAL_MAIN_MOVE_ASSISTANT_COST = 3;
+	public static final int KING_INITIAL_CITY_INDEX = 9;
 
 	//color distributions
 	public static final CityColor[][] CITIES_COLOR = {
